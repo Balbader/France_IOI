b35ee70cc3dafcabc20ef0cef8fab5d9832fa41f
Balbader <15971200+Balbader@users.noreply.github.com>
2020-10-08 09:53:46 +0200

    Initial commit

--- Niveau 1/3.Calculs et découverte de variables/12.Construction d'une pyramide/construction_d'une_pyramide.c
+++ Niveau 1/3.Calculs et découverte de variables/12.Construction d'une pyramide/construction_d'une_pyramide.c
@@ -1,3 +1,30 @@
 #include <unistd.h>
 
+void    putchar(char c)
+{
+    write(1, &c, 1);
+}
 
+void    print_nb(int nb)
+{
+    if (nb >= 10)
+        print_nb(nb / 10);
+    nb = nb % 10 + '0';
+    putchar (nb);
+}
+
+int     main(void)
+{
+    int     cube;
+    int     tot_cube;
+    
+    cube = 17;
+    tot_cube = 0;
+    while (cube >= 1)
+    {
+        tot_cube  += cube * cube * cube;
+        cube = cube - 2;
+    }
+    print_nb(tot_cube);
+    return (0);
+}
