6954f54041e6d646ed6bbeef0415584041734f9c
Balbader <15971200+Balbader@users.noreply.github.com>
2020-10-13 17:29:38 +0200

    Initial commit

--- Niveau 1/6.Structures avancées/2.Planning de la journée/planning_de_la_journee.c
+++ Niveau 1/6.Structures avancées/2.Planning de la journée/planning_de_la_journee.c
@@ -5,7 +5,23 @@ int     main(void)
     int     pos;
     int     cities;
     int     cit_pos;
-
-
+    int     dist;
+    int     sleep;
+ 
+    scanf("%d", &pos);
+    scanf("%d", &cities);
+    cit_pos = 0;
+    sleep = 0;
+    while (cities >= 1)
+    {
+        scanf("%d", &cit_pos);
+        dist = pos - cit_pos;
+        if (dist < 0)
+            dist *= -1;
+        if (dist <= 50)
+            sleep += 1;
+        cities--;
+    }
+    printf("%d\n", sleep);
     return (0);
 }
