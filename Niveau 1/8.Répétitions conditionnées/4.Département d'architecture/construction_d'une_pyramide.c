1d4d1f099349a3c2aa26d9d1485e393b472ddfb0
Balbader <15971200+Balbader@users.noreply.github.com>
2020-10-20 21:46:53 +0200

    Initial commit

---
+++ Niveau 1/8.Répétitions conditionnées/4.Département d'architecture/construction_d'une_pyramide.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+// 1 + 3; 3 + (3 * 2); 
+
+int     main(void)
+{
+    int     tot_stone;
+    int     height;
+    int     stone_nbr;
+    int     start;
+
+    tot_stone = 0;
+    height = 0;
+    stone_nbr = 1;
+    start = 3;
+    scanf("%d", &tot_stone);
+    while (tot_stone != 0)
+    {
+        stone_nbr 
+
+        height++;
+    }
+}
+
