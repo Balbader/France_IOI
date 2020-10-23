d18a1a2e79a30b99bdb466e7e25b38f9fcb18da3
Balbader <15971200+Balbader@users.noreply.github.com>
2020-10-23 13:27:33 +0200

    Initial commit

---
+++ Niveau 2/2.Découverte des tableaux/1.Préparation de l'onguent/préparation_onguent.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int     main(void)
+{
+    int     arr[10] = {500, 180, 650, 25, 666, 42, 421, 1, 370, 211};
+    int     nbr;
+
+    scanf("%d", &nbr);
+    printf("%d\n", arr[nbr]);
+    return (0);
+}
