e998b59f72dc01c870772274da54909db2435b96
Balbader <15971200+Balbader@users.noreply.github.com>
2020-11-02 08:48:23 +0100

    Initial commit

---
+++ Niveau 2/3.Chaines de caracteres/4.Résumé de livres/résumé_de_livre.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int     main(void)
+{
+    int     books;
+    int     min_length;
+    char    title[1000];
+    char    summary[1000];
+    int     i;
+
+    scanf("%d%d\n", &books, &min_length);
+    for (i = 1; i <= books; i++)
+    {
+        scanf("%[^\n]\n", title);
+        scanf("%[^\n]\n", summary);
+        if (ft_strlen(summary) < min_length)
+            printf("%s\n", title);
+    }
+    return (0);
+}
