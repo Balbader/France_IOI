6e828cd827a213af5717b143dc5a97fa1963cf6d
Balbader <15971200+Balbader@users.noreply.github.com>
2020-11-05 17:08:33 +0100

    Initial commit

---
+++ Niveau 2/4.Fonctions/6.Phénomene numérique/phénomene_numérique.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int     ft_next_term(int);
+
+int     main(void)
+{
+    int     term;
+    int     next;
+
+    scanf("%d", &term);
+    next = ft_next_term(term);
+    while (next != 1)
+    {
+        printf("%d ", next);
+        term = next;
+        next = ft_next_term(term);
+    }
+    if (next == 1)
+        printf("%d\n", next);
+    return (0);
+}
+
+int     ft_next_term(int x)
+{
+    int     next;
+    if (x % 2 == 0)
+        next = x / 2;
+    else
+        next = (x * 3) + 1;
+    return (next);
+}
