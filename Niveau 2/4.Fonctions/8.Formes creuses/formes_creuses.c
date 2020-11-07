11be146db6ed69f87909934df48e87500819fea0
Balbader <15971200+Balbader@users.noreply.github.com>
2020-11-07 17:50:21 +0100

    Initial commit

--- Niveau 2/4.Fonctions/8.Formes creuses/formes_creuses.c
+++ Niveau 2/4.Fonctions/8.Formes creuses/formes_creuses.c
@@ -1,6 +1,101 @@
+#include <unistd.h>
 #include <stdio.h>
+
+void    ft_putchar(char);
+void    ft_print_X(char);
+void    ft_print_rec(int, int);
+void    ft_print_tri(int);
 
 int     main(void)
 {
+    int     x;
+    int     y;
+    int     nb;
+
+    x = 0;
+    y = 0;
+    nb = 0;
+    ft_print_X('X');
+    ft_putchar('\n');
+    ft_putchar('\n');
+    ft_print_rec(x, y);
+    ft_putchar('\n');
+    ft_putchar('\n');
+    ft_print_tri(nb);
     return (0);
 }
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_print_X(char c)
+{
+    int     nb;
+
+    scanf("%d", &nb);
+    while (nb > 0)
+    {
+        ft_putchar(c);
+        nb--;
+    }
+}
+
+void    ft_print_rec(int x, int y)
+{
+    int     i;
+
+    scanf("%d%d", &x, &y);
+    for (i = 0; i < y; i++)
+        ft_putchar('#');
+    ft_putchar('\n');
+    while (x - 1 > 1)
+    {
+        ft_putchar('#');
+        for (i = 1; i < y - 1; i++)
+            ft_putchar(' ');
+        ft_putchar('#');
+        ft_putchar('\n');
+        x--;
+    }
+    for (i = 0; i < y; i++)
+        ft_putchar('#');
+}
+
+void    ft_print_tri(int nb)
+{
+    int     i;
+    int     y;
+
+    scanf("%d", &nb);
+    y = 0;;
+    for (i = 0; i < nb; i++)
+    {
+        while (y < nb)
+        {
+            ft_putchar('@');
+            y++;
+        }
+        y = 0;
+        ft_putchar('\n');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
