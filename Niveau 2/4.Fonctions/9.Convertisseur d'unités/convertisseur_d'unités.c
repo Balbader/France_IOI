931bb5d8d04a46712c327a3ff22a12646c3fb7a8
Balbader <15971200+Balbader@users.noreply.github.com>
2020-11-09 11:52:02 +0100

    Update

--- Niveau 2/4.Fonctions/9.Convertisseur d'unités/convertisseur_d'unités.c
+++ Niveau 2/4.Fonctions/9.Convertisseur d'unités/convertisseur_d'unités.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
-#include <unistd.h>
 
 int     main(void)
 {
-    return (0);
+    int     nbr_of_entries;
+    int     i;
+    double  imput;
+    char    type;
+    char    p;
+    char    l;
+    char    f;
+
+    p = 'p';
+    l = 'l';
+    f = 'f';
+
+    scanf("%d", &nbr_of_entries);
+
+    for (i = 0; i < nbr_of_entries; i++)
+    {
+        scanf("%lf %c", &imput, &type);
+        
+        if (type == 'm')
+        {
+            double  foot;
+            foot = imput / 0.3048;
+            printf("%lf %c\n", foot, p);
+        }
+        if (type == 'g')
+        {
+            double  pds;
+            pds = imput * 0.002205;
+            printf("%lf %c\n", pds, l);
+        }
+        if (type == 'c')
+        {
+            double  far;
+            far = (1.8 * imput) + 32;
+            printf("%lf %c\n", far, f);
+        }
+
+        imput = 0;
+        type = ' ';
+    }
+    return 0;
 }
+
