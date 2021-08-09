a276e55c0981d77e5b7dd13010a3bc22c195b616
Balbader <15971200+Balbader@users.noreply.github.com>
2021-08-09 17:42:08 +0200

    update

--- Niveau 1/7.Conditions avancées, opérateurs booléens/7.Personne disparue/personne_disparue.c
+++ Niveau 1/7.Conditions avancées, opérateurs booléens/7.Personne disparue/personne_disparue.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int     main(void)
+int main(void)
 {
-    int     missing;
-    int     list;
-    int     check_list;
-    bool     not_found;
+    int missing;
+    int list;
+    int check_list;
+    bool not_found;
 
     scanf("%d%d", &missing, &list);
     not_found = false;
@@ -23,3 +23,38 @@ int     main(void)
         printf("Encore dans la ville\n");
     return (0);
 }
+
+/*
+#include <stdio.h>
+
+int     main(void)
+{
+    int tag;
+    int list;
+    int tags;
+    int i = 0;
+    int count = 0;
+
+    scanf("%d", &tag);
+    scanf("%d", &list);
+    while (i < list)
+    {
+        scanf("%d", &tags);
+        if (tags == tag)
+        {
+            count += 1;
+        }
+        list--;
+    }
+
+    if (count > 0)
+    {
+        printf("Sorti de la ville\n");
+    }
+    else
+    {
+        printf("Encore dans la ville\n");
+    }
+    return (0);
+}
+*/
