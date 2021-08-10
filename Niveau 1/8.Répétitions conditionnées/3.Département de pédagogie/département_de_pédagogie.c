672603c82774835753bafcbd6ade9df43949ebcf
Balbader <15971200+Balbader@users.noreply.github.com>
2021-08-10 19:03:57 +0200

    update

--- Niveau 1/8.Répétitions conditionnées/3.Département de pédagogie/département_de_pédagogie.c
+++ Niveau 1/8.Répétitions conditionnées/3.Département de pédagogie/département_de_pédagogie.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int     main(void)
+int main(void)
 {
-    int     nbr_to_find;
-    int     try;
-    int     i;
+    int nbr_to_find;
+    int try;
+    int i;
 
     try = 0;
     i = 0;
@@ -21,3 +21,56 @@ int     main(void)
     printf("Nombre d'essais nécessaires :\n%d\n", try);
     return (0);
 }
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int nbr;
+    int try;
+    int count = 1;
+
+    scanf("%d", &nbr);
+    scanf("%d", &try);
+    if (try < nbr)
+    {
+        printf("c'est plus\n");
+        count++;
+    }
+    else if (try > nbr)
+    {
+        printf("c'est moins\n");
+        count++;
+    }
+    else if (try == nbr)
+    {
+        printf("Nombre d'essais nécessaires :\n");
+        printf("%d\n", count);
+    }
+
+    while (try != nbr)
+    {
+        scanf("%d", &try);
+        if (try < nbr)
+        {
+            printf("c'est plus\n");
+            count++;
+        }
+        else if (try > nbr)
+        {
+            printf("c'est moins\n");
+            count++;
+        }
+        else if (try == nbr)
+        {
+            printf("Nombre d'essais nécessaires :\n");
+            printf("%d\n", count);
+            break ;
+        }
+    }
+
+    return (0);
+}
+
+*/
