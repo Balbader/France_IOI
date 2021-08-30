dfe065d9d3136c8b60ec8246f785f677f0684add
Balbader <15971200+Balbader@users.noreply.github.com>
2021-08-30 17:58:36 +0200

    update

--- Niveau 2/2.Découverte des tableaux/3.Grand inventaire/grand_inventaire.c
+++ Niveau 2/2.Découverte des tableaux/3.Grand inventaire/grand_inventaire.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int     main(void)
+int main(void)
 {
-    int     nb_op;
-    int     stock[11] = {0};
-    int     num;
-    int     qte;
-    int     i;
+    int nb_op;
+    int stock[11] = {0};
+    int num;
+    int qte;
+    int i;
 
     scanf("%d", &nb_op);
     while (nb_op > 0)
@@ -19,3 +19,33 @@ int     main(void)
         printf("%d\n", stock[i]);
     return (0);
 }
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int tot_op;
+    int ingredient_nbr;
+    int qte;
+    int i = 0;
+    int arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    scanf("%d", &tot_op);
+    while (tot_op > 0)
+    {
+        scanf("%d", &ingredient_nbr);
+        scanf("%d", &qte);
+        arr[ingredient_nbr - 1] += qte;
+        tot_op--;
+    }
+
+    while (i < 10)
+    {
+        printf("%d\n", arr[i]);
+        i++;
+    }
+    return (0);
+}
+
+*/
