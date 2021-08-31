a01878680a8de9f1c5bc1d326156adc85339082d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-08-31 16:05:19 +0200

    update

--- Niveau 2/2.Découverte des tableaux/4.Étude de marché/étude_de_marché.c
+++ Niveau 2/2.Découverte des tableaux/4.Étude de marché/étude_de_marché.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int     main(void)
+int main(void)
 {
-    int     nbr_of_prod;
-    int     nbr_of_pers;
-    int     fav_prod;
-    int     prod_list[1000] = {0};
-    int     i;
+    int nbr_of_prod;
+    int nbr_of_pers;
+    int fav_prod;
+    int prod_list[1000] = {0};
+    int i;
 
     scanf("%d %d", &nbr_of_prod, &nbr_of_pers);
     while (nbr_of_pers > 0)
@@ -19,3 +19,33 @@ int     main(void)
         printf("%d\n", prod_list[i]);
     return (0);
 }
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int tot_prod;
+    int tot_peeps;
+    int prod_nbr;
+    int arr[1000] = {0};
+    int i = 0;
+    int j = 0;
+
+    scanf("%d", &tot_prod);
+    scanf("%d", &tot_peeps);
+
+    while (i < tot_peeps)
+    {
+        scanf("%d", &prod_nbr);
+        arr[prod_nbr] += 1;
+        i++;
+    }
+    while (j < tot_prod)
+    {
+        printf("%d\n", arr[j]);
+        j++;
+    }
+    return (0);
+}
+*/
