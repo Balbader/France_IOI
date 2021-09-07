663cde8d727f89b278babc5280efa6131ff5f40f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-07 18:14:56 +0200

    update

--- Niveau 2/2.Découverte des tableaux/7.Journée de cadeaux/journée_de_cadeaux.c
+++ Niveau 2/2.Découverte des tableaux/7.Journée de cadeaux/journée_de_cadeaux.c
@@ -3,20 +3,20 @@
 
 int compareEntier(const void *pEntier1, const void *pEntier2)
 {
-  return ( *(int*)(pEntier1) - *(int*)(pEntier2) );
+    return (*(int *)(pEntier1) - *(int *)(pEntier2));
 }
 
 int main(void)
 {
-    int     tot_hab;
-    int     revenue[1000];
-    int     fortune_par_habitant;
-    int     moy_impair;
-    int     moy_pair;
-    int     moy_pair_2;
-    int     moy_totale;
-    int     i;
-    float   resultat_pair;
+    int tot_hab;
+    int revenue[1000];
+    int fortune_par_habitant;
+    int moy_impair;
+    int moy_pair;
+    int moy_pair_2;
+    int moy_totale;
+    int i;
+    float resultat_pair;
 
     scanf("%d", &tot_hab);
     for (i = 0; i < tot_hab; i++)
@@ -33,10 +33,57 @@ int main(void)
     else
     {
         moy_pair = tot_hab / 2;
-        moy_pair_2 = (tot_hab / 2) - 1;        
+        moy_pair_2 = (tot_hab / 2) - 1;
         moy_totale = revenue[moy_pair] + revenue[moy_pair_2];
         resultat_pair = moy_totale / 2.0;
-        printf("%f\n", resultat_pair); 
+        printf("%f\n", resultat_pair);
     }
     return 0;
 }
+
+/*
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int cmp(const void *a, const void *b)
+{
+    return ( *(int*)a - *(int*)b );
+}
+
+
+int main(void)
+{
+    int tot_hab;
+    int wealth;
+    int i = 0;
+    int arr[1000] = {0};
+    double res;
+
+    scanf("%d", &tot_hab);
+
+    while (i < tot_hab)
+    {
+        scanf("%d", &wealth);
+        arr[i] += wealth;
+        i++;
+    }
+
+    qsort(arr, tot_hab, sizeof(int), cmp);
+
+    if (tot_hab % 2 == 0)
+    {
+        tot_hab /= 2;
+        res = arr[tot_hab - 1] + arr[tot_hab];
+        printf("%f\n", res / 2);
+    }
+    else
+    {
+        printf("%d\n", arr[tot_hab/2]);
+    }
+
+    return (0);
+}
+
+*/
