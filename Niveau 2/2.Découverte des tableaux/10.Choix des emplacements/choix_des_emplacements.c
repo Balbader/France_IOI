ca0cc66b0e9b6138ab22cafba19bc5cfbc040d77
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-09 16:42:17 +0200

    update

--- Niveau 2/2.Découverte des tableaux/10.Choix des emplacements/choix_des_emplacements.c
+++ Niveau 2/2.Découverte des tableaux/10.Choix des emplacements/choix_des_emplacements.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int     list[1000];
-    int     places;
-    int     seller;
-    int     i;
-    
+    int list[1000];
+    int places;
+    int seller;
+    int i;
+
     scanf("%d", &places);
     for (i = 0; i < places; i++)
     {
@@ -18,3 +18,32 @@ int main(void)
     return 0;
 }
 
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int tot_spots;
+    int arr[1000] = {0};
+    int seller_nbr;
+    int i = 0;
+    int j = 0;
+
+    scanf("%d", &tot_spots);
+
+    while (i < tot_spots)
+    {
+        scanf("%d", &seller_nbr);
+        arr[seller_nbr] = i;
+        i++;
+    }
+
+    while (j < tot_spots)
+    {
+        printf("%d\n", arr[j]);
+        j++;
+    }
+    return (0);
+}
+
+*/
