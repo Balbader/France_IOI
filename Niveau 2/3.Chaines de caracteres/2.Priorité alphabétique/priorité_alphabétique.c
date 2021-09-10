242bb2e6a30c77b29c0c4e5589d38659795ff4e5
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-10 15:47:22 +0200

    update

--- Niveau 2/3.Chaines de caracteres/2.Priorité alphabétique/priorité_alphabétique.c
+++ Niveau 2/3.Chaines de caracteres/2.Priorité alphabétique/priorité_alphabétique.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void    ft_putstr(char *str)
+void ft_putstr(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -13,9 +13,9 @@ void    ft_putstr(char *str)
     }
 }
 
-int     ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (s1[i] && s2[i] && s1[i] == s2[i])
@@ -23,11 +23,11 @@ int     ft_strcmp(char *s1, char *s2)
     return (s1[i] - s2[i]);
 }
 
-int     main(void)
+int main(void)
 {
-    char    str1[51];
-    char    str2[51];
-    int     res;
+    char str1[51];
+    char str2[51];
+    int res;
 
     scanf("%[^\n]\n", str1);
     scanf("%[^\n]", str2);
@@ -40,3 +40,43 @@ int     main(void)
         return (0);
     return (0);
 }
+
+/*
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int     ft_strcmp(char *s1, char *s2)
+{
+    int i = 0;
+    while (s1[i] == s2[i] && s1[i] && s2[i])
+    {
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
+int main(void)
+{
+    char name_1[51];
+    char name_2[51];
+    scanf("%[^\n]\n", name_1);
+    scanf("%[^\n]\n", name_2);
+    ft_strcmp(name_1, name_2);
+    if (ft_strcmp(name_1, name_2) == 0)
+    {
+        return (0);
+    }
+    if (ft_strcmp(name_1, name_2) > 0)
+    {
+        printf("%s\n", name_2);
+    }
+    else
+    {
+        printf("%s\n", name_1);
+    }
+
+    return (0);
+}
+
+*/
