53430f5fbd04979934d091859d3d6d1acc6882ab
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-13 11:14:16 +0200

    update

--- Niveau 2/3.Chaines de caracteres/3.Une ligne sur deux/une_ligne_sur_deux.c
+++ Niveau 2/3.Chaines de caracteres/3.Une ligne sur deux/une_ligne_sur_deux.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void    ft_putstr(char *str)
+void ft_putstr(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -13,11 +13,11 @@ void    ft_putstr(char *str)
     }
 }
 
-int     main(void)
+int main(void)
 {
-    int     lines;
-    char    text[101];
-    int     i;
+    int lines;
+    char text[101];
+    int i;
 
     scanf("%d\n", &lines);
     for (i = 0; i < lines; i++)
@@ -31,3 +31,35 @@ int     main(void)
     }
     return (0);
 }
+
+/*
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char arr[101][101];
+    char temp[101];
+    int lines;
+    int i = 0;
+    int j = 0;
+
+    scanf("%d\n", &lines);
+    while (i < lines)
+    {
+        scanf("%[^\n]\n", temp);
+        strcpy(arr[i], temp);
+        i++;
+    }
+
+    while (j < lines)
+    {
+        printf("%s\n", arr[j]);
+        j += 2;
+    }
+    return (0);
+}
+
+*/
