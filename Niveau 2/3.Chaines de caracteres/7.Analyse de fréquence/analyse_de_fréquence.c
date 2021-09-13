252ed4111e0fb9e51acabe4707d0d41e18a56f15
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-13 20:05:46 +0200

    update

--- Niveau 2/3.Chaines de caracteres/7.Analyse de fréquence/analyse_de_fréquence.c
+++ Niveau 2/3.Chaines de caracteres/7.Analyse de fréquence/analyse_de_fréquence.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int     ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -10,15 +10,15 @@ int     ft_strlen(char *str)
     return (i);
 }
 
-int     main(void)
+int main(void)
 {
-    int     lines;
-    int     words;
-    int     length;
-    int     arr[101] = {0};
-    char    str[101] = {0};
-    int     i;
-    int     j;
+    int lines;
+    int words;
+    int length;
+    int arr[101] = {0};
+    char str[101] = {0};
+    int i;
+    int j;
 
     scanf("%d %d", &lines, &words);
     i = 0;
@@ -42,3 +42,63 @@ int     main(void)
     return (0);
 }
 
+/*
+#include <unistd.h>
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+
+    return (i);
+}
+
+int main(void)
+{
+    int     tot_lines;
+    int     tot_words;
+    int     count;
+    int     i;
+    int     j;
+    char    str[1001];
+    char    arr[1001];
+
+    count = 0;
+    i = 0;
+
+    scanf("%d", &tot_lines);
+    scanf("%d\n", &tot_words);
+
+    while (i < tot_lines)
+    {
+        j = 0;
+        while (j < tot_words)
+        {
+            scanf("%s", str);
+            count = ft_strlen(str);
+            arr[count] += 1;
+            j++;
+
+        }
+        i++;
+    }
+
+    i = 0;
+    while (i < 101)
+    {
+        if (arr[i] > 0)
+        {
+            printf("%d : %d\n", i , arr[i]);
+        }
+        i++;
+    }
+
+    return (0);
+}
+*/
