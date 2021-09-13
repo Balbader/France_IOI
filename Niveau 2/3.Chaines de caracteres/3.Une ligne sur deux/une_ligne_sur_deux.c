6815b130e17f02aeec5f5883b3876819c74d45a4
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-13 12:49:22 +0200

    update

--- Niveau 2/3.Chaines de caracteres/3.Une ligne sur deux/une_ligne_sur_deux.c
+++ Niveau 2/3.Chaines de caracteres/3.Une ligne sur deux/une_ligne_sur_deux.c
@@ -61,5 +61,22 @@ int main(void)
     }
     return (0);
 }
+*/
 
+/*
+#include <stdio.h>
+int main()
+{
+   int nbLignes;
+   char ligne[101];
+   scanf("%d\n", &nbLignes);
+   for (int idLigne = 1; idLigne <= nbLignes; idLigne = idLigne + 1)
+   {
+      scanf("%[^\n]\n", ligne);
+      if (idLigne % 2 == 1)
+      {
+         printf("%s\n", ligne);
+      }
+   }
+}
 */
