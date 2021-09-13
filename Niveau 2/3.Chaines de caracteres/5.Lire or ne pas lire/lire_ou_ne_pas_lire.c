67395fd5f88ba964241ffcdc4806f739299ac44b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-13 14:58:54 +0200

    update

--- Niveau 2/3.Chaines de caracteres/5.Lire or ne pas lire/lire_ou_ne_pas_lire.c
+++ Niveau 2/3.Chaines de caracteres/5.Lire or ne pas lire/lire_ou_ne_pas_lire.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int     ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -10,12 +10,12 @@ int     ft_strlen(char *str)
     return (i);
 }
 
-int     main(void)
+int main(void)
 {
-    int     nbr_books;
-    char    title[1001];
-    int     length;
-    int     i;
+    int nbr_books;
+    char title[1001];
+    int length;
+    int i;
 
     scanf("%d\n", &nbr_books);
     length = 0;
@@ -30,3 +30,41 @@ int     main(void)
     }
     return (0);
 }
+
+/*
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i])
+    {
+        i++;
+    }
+    return (i);
+}
+
+int main(void)
+{
+    int tot_books;
+    int temp = 0;
+    char title[1001];
+
+    scanf("%d", &tot_books);
+    while (tot_books > 0)
+    {
+        scanf("%[^\n]\n", title);
+        if (ft_strlen(title) > temp)
+        {
+            temp = ft_strlen(title);
+            printf("%s\n", title);
+        }
+        tot_books--;
+    }
+    return (0);
+}
+*/
