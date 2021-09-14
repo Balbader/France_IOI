478fb5856bb320923ea08f57d14c18905000bd7e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-14 18:32:39 +0200

    update

--- Niveau 2/3.Chaines de caracteres/10.Inscription d'étudiants/inscription_d'étudiants.c
+++ Niveau 2/3.Chaines de caracteres/10.Inscription d'étudiants/inscription_d'étudiants.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void    check_name(char *str);
+void check_name(char *str);
 
-int     main(void)
+int main(void)
 {
-    char    str[51];
+    char str[51];
 
     scanf("%[^\n]", str);
     check_name(str);
     return (0);
 }
 
-void    check_name(char *str)
+void check_name(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     if (str[i] <= 'F')
@@ -23,3 +23,33 @@ void    check_name(char *str)
     if (str[i] >= 'Q' && str[i] <= 'Z')
         printf("3");
 }
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int     main(void)
+{
+    char    name[51];
+    scanf("%s", name);
+    if (name[0] >= 'A' && name[0] <= 'F')
+    {
+        ft_putchar('1');
+    }
+    else if (name[0] >= 'G' && name[0] <= 'P')
+    {
+        ft_putchar('2');
+    }
+    else
+    {
+        ft_putchar('3');
+    }
+    return (0);
+}
+
+*/
