d3dbcd3a6516ef0c5220d227e457a9ca1f6b0318
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-14 18:10:04 +0200

    update

--- Niveau 2/3.Chaines de caracteres/9.Ecriture en miroir/écriture_en_miroir.c
+++ Niveau 2/3.Chaines de caracteres/9.Ecriture en miroir/écriture_en_miroir.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void    ft_putchar(char c);
-int     ft_strlen(char *str);
+void ft_putchar(char c);
+int ft_strlen(char *str);
 
-int     main(void)
+int main(void)
 {
-    char    str[1001];
-    int     lines;
-    int     length;
-    int     i;
+    char str[1001];
+    int lines;
+    int length;
+    int i;
 
     scanf("%d\n", &lines);
     while (lines > 0)
@@ -29,17 +29,38 @@ int     main(void)
     return (0);
 }
 
-void    ft_putchar(char c)
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-int     ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
         i++;
     return (i);
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   int nbLignes;
+   scanf("%d\n", &nbLignes);
+   for (int idLigne = 0; idLigne < nbLignes; idLigne = idLigne + 1)
+   {
+      char ligneTexte[1001];
+      scanf("%[^\n]\n",ligneTexte);
+      int longueur = strlen(ligneTexte);
+      for (int iCar = longueur  - 1; iCar >= 0; iCar = iCar - 1)
+      {
+         printf("%c", ligneTexte[iCar]);
+      }
+      printf("\n");
+   }
+}
+*/
