120f5bdd743d28fa732c8925fc5d09d80c27468a
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-16 16:41:12 +0200

    update

--- Niveau 2/3.Chaines de caracteres/12.La bataille/la_bataille.c
+++ Niveau 2/3.Chaines de caracteres/12.La bataille/la_bataille.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void    ft_check_equal(char *s1, char *s2);
-void    ft_check_error(char *s1, char *s2);
-void    ft_strcmp(char *s1, char *s2);
+void ft_check_equal(char *s1, char *s2);
+void ft_check_error(char *s1, char *s2);
+void ft_strcmp(char *s1, char *s2);
 
-int     main(void)
+int main(void)
 {
-    char    str1[53];
-    char    str2[53];
-    int     pos;
-    int     i;
-    int     j;
+    char str1[53];
+    char str2[53];
+    int pos;
+    int i;
+    int j;
 
     i = 0;
     j = 0;
@@ -55,3 +55,105 @@ int     main(void)
     }
     return (0);
 }
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int     main(void)
+{
+    char    player_1[53];
+    char    player_2[53];
+    int     count;
+    int     i;
+    int     j;
+
+    i = 0;
+    j = 0;
+    count = 0;
+
+    scanf("%s\n", player_1);
+    scanf("%s", player_2);
+
+    while (player_1[i] && player_2[i] && player_1[i] == player_2[i])
+    {
+        count++;
+        i++;
+        j++;
+    }
+    if (player_1[i] == player_2[i])
+    {
+        printf("=\n");
+        printf("%d\n", count);
+        return (0);
+    }
+    if (player_1[i] == '\0')
+    {
+        printf("%d\n", 2);
+        printf("%d\n", count);
+        return (0);
+    }
+
+    if (player_2[j] == '\0' )
+    {
+        printf("%d\n", 1);
+        printf("%d\n", count);
+        return (0);
+    }
+
+    if (player_1[i] < player_2[i])
+    {
+        printf("%d\n", 1);
+        printf("%d\n", count);
+        return (0);
+    }
+
+    if (player_1[i] > player_2[i])
+    {
+        printf("%d\n", 2);
+        printf("%d\n", count);
+        return (0);
+    }
+    return (0);
+}
+*/
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int     main(void)
+{
+    char    player_1[53];
+    char    player_2[53];
+    int     count;
+
+    count = 0;
+
+    scanf("%s\n", player_1);
+    scanf("%s", player_2);
+
+    while (count < strlen(player_1) && count < strlen(player_2)
+            && player_1[count] == player_2[count])
+    {
+        count++;
+    }
+    if (count == strlen(player_1) && count == strlen(player_2))
+    {
+        printf("=\n");
+    }
+    else if (count == strlen(player_2)
+            || (count < strlen(player_1) && player_1[count] < player_2[count]))
+    {
+        printf("1\n");
+    }
+    else
+    {
+        printf("2\n");
+    }
+    printf("%d\n", count);
+    return (0);
+}
+*/
