d87cfc674cbc69c78a1a46ee82a9dabbd1218c14
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-16 20:03:25 +0200

    update

--- Niveau 2/3.Chaines de caracteres/13.Analyse d'une langue/analyse_une_langue.c
+++ Niveau 2/3.Chaines de caracteres/13.Analyse d'une langue/analyse_une_langue.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int     ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -10,14 +10,14 @@ int     ft_strlen(char *str)
     return (i);
 }
 
-int     main(void)
+int main(void)
 {
-    char    to_find;
-    char    str[1001];
-    int     lines;
-    int     found;
-    int     i;
-    int     j;
+    char to_find;
+    char str[1001];
+    int lines;
+    int found;
+    int i;
+    int j;
 
     scanf("%c", &to_find);
     scanf("%d\n", &lines);
@@ -34,3 +34,52 @@ int     main(void)
     printf("%d\n", found);
     return (0);
 }
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int     main(void)
+{
+    char    letter;
+    int     lines;
+    char    str[1001][1001];
+    char    temp[1001];
+    int     i;
+    int     j;
+    int     count;
+
+    i = 0;
+    j = 0;
+    count = 0;
+
+    scanf("%c", &letter);
+    scanf("%d\n", &lines);
+
+    while (i < lines)
+    {
+        scanf("%[^\n]\n", temp);
+        strcpy(str[i], temp);
+        i++;
+    }
+
+    i = 0;
+    while (i < (sizeof(str) / sizeof(str[0])))
+    {
+        j = 0;
+        while (str[i][j] != '\0')
+        {
+            if (str[i][j] == letter)
+            {
+                count++;
+            }
+            j++;
+        }
+        i++;
+    }
+
+    printf("%d\n", count);
+    return (0);
+}
+*/
