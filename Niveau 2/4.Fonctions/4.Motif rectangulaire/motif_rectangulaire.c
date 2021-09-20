e0acf50300bbd38b5b6542be7689490b2457ea6a
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-20 10:31:10 +0200

    update

--- Niveau 2/4.Fonctions/4.Motif rectangulaire/motif_rectangulaire.c
+++ Niveau 2/4.Fonctions/4.Motif rectangulaire/motif_rectangulaire.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void    ft_putchar(char c);
+void ft_putchar(char c);
 
-int     main(void)
+int main(void)
 {
-    int     x;
-    int     y;
-    char    c;
-    int     i;
-    int     j;
+    int x;
+    int y;
+    char c;
+    int i;
+    int j;
 
     scanf("%d%d\n", &x, &y);
     scanf("%c", &c);
@@ -29,7 +29,45 @@ int     main(void)
     return (0);
 }
 
-void    ft_putchar(char c)
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
+
+/*
+#include <stdio.h>
+
+void    print_char(int x, int y, char c)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < x)
+    {
+        j = 0;
+        while (j < y)
+        {
+            printf("%c", c);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
+
+int     main(void)
+{
+    int     x;
+    int     y;
+    char    c;
+
+    scanf("%d", &x);
+    scanf("%d\n", &y);
+    scanf("%c", &c);
+
+    print_char(x, y, c);
+
+    return (0);
+}
+*/
