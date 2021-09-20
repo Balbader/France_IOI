e81ee471c6dec49a0bfa8a5e4870657dc60377ac
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-20 10:17:42 +0200

    update

--- Niveau 2/4.Fonctions/3.Dentelle/dentelle.c
+++ Niveau 2/4.Fonctions/3.Dentelle/dentelle.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void    ft_putchar(char c);
-void    ft_print(char c, int len);
+void ft_putchar(char c);
+void ft_print(char c, int len);
 
-int     main(void)
+int main(void)
 {
-    int     len;
+    int len;
 
     scanf("%d", &len);
     ft_print('X', len);
@@ -15,14 +15,14 @@ int     main(void)
     return (0);
 }
 
-void    ft_putchar(char c)
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-void    ft_print(char c, int len)
+void ft_print(char c, int len)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (i < len)
@@ -32,3 +32,70 @@ void    ft_print(char c, int len)
     }
     ft_putchar('\n');
 }
+
+/*
+#include <stdio.h>
+
+void    print_char(int nbr)
+{
+    int i = 0;
+
+    scanf("%d", &nbr);
+
+    while (i < nbr)
+    {
+        printf("X");
+        i++;
+    }
+    printf("\n");
+    i = 0;
+    while (i < nbr)
+    {
+        printf("#");
+        i++;
+    }
+    printf("\n");
+    i = 0;
+    while (i < nbr)
+    {
+        printf("i");
+        i++;
+    }
+    printf("\n");
+}
+
+int     main(void)
+{
+    int nbr;
+    print_char(nbr);
+    return (0);
+}
+*/
+
+/*
+#include <stdio.h>
+
+void    print_char(int nbr, char c)
+{
+    int i = 0;
+    while (i < nbr)
+    {
+        printf("%c", c);
+        i++;
+    }
+}
+
+int     main(void)
+{
+    int nbr;
+    scanf("%d", &nbr);
+    print_char(nbr, 'X');
+    printf("\n");
+    print_char(nbr, '#');
+    printf("\n");
+    print_char(nbr, 'i');
+    printf("\n");
+    return (0);
+}
+
+*/
