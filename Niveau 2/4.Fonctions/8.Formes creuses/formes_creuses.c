05af8d4781fba3ef89c12596c91494126821691f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-22 10:38:16 +0200

    update

--- Niveau 2/4.Fonctions/8.Formes creuses/formes_creuses.c
+++ Niveau 2/4.Fonctions/8.Formes creuses/formes_creuses.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void    ft_putchar(char);
-void    ft_print_X(char);
-void    ft_print_rec(int, int);
-void    ft_print_tri(int);
+void ft_putchar(char);
+void ft_print_X(char);
+void ft_print_rec(int, int);
+void ft_print_tri(int);
 
-int     main(void)
+int main(void)
 {
-    int     x;
-    int     y;
-    int     nb;
+    int x;
+    int y;
+    int nb;
 
     x = 0;
     y = 0;
@@ -25,14 +25,14 @@ int     main(void)
     return (0);
 }
 
-void    ft_putchar(char c)
+void ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-void    ft_print_X(char c)
+void ft_print_X(char c)
 {
-    int     nb;
+    int nb;
 
     scanf("%d", &nb);
     while (nb > 0)
@@ -42,9 +42,9 @@ void    ft_print_X(char c)
     }
 }
 
-void    ft_print_rec(int x, int y)
+void ft_print_rec(int x, int y)
 {
-    int     i;
+    int i;
 
     scanf("%d%d", &x, &y);
     if (x == 1)
@@ -82,10 +82,10 @@ void    ft_print_rec(int x, int y)
         ft_putchar('#');
 }
 
-void    ft_print_tri(int nb)
+void ft_print_tri(int nb)
 {
-    int     i;
-    int     y;
+    int i;
+    int y;
 
     scanf("%d", &nb);
     y = 0;
@@ -110,3 +110,61 @@ void    ft_print_tri(int nb)
     }
     ft_putchar('\n');
 }
+
+/*
+#include <stdio.h>
+void ligne(int longueur, char motif)
+{
+   for (int iCol = 1; iCol <= longueur; iCol = iCol + 1)
+   {
+      printf("%c", motif);
+   }
+   printf("\n");
+}
+void ligneCreuse(int longueur, char motif)
+{
+   if (longueur > 1)
+   {
+      printf("%c", motif);
+      for (int iCol = 2; iCol < longueur; iCol = iCol + 1)
+      {
+         printf(" ");
+      }
+   }
+   printf("%c\n", motif);
+}
+void rectangle(int hauteur, int longueur, char motif)
+{
+   if (hauteur > 1)
+   {
+      ligne(longueur, motif);
+      for (int iLigne = 2; iLigne < hauteur; iLigne = iLigne + 1)
+      {
+         ligneCreuse(longueur, motif);
+      }
+   }
+   ligne(longueur, motif);
+}
+void triangle(int hauteur, char motif)
+{
+   for (int iLigne = 1; iLigne < hauteur; iLigne = iLigne + 1)
+   {
+      ligneCreuse(iLigne, motif);
+   }
+   ligne(hauteur, motif);
+}
+int main()
+{
+   int longueurLigne;
+   scanf("%d", &longueurLigne);
+   ligne(longueurLigne, 'X');
+   printf("\n");
+   int hauteurRectangle, largeurRectangle;
+   scanf("%d%d", &hauteurRectangle, &largeurRectangle);
+   rectangle(hauteurRectangle, largeurRectangle, '#');
+   printf("\n");
+   int hauteurTriangle;
+   scanf("%d", &hauteurTriangle);
+   triangle(hauteurTriangle, '@');
+}
+*/
