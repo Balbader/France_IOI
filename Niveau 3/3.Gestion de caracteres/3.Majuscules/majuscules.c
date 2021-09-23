9a60fecc417ecc093fa52d8a2b7083e30567719d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-23 19:07:51 +0200

    update

--- Niveau 3/3.Gestion de caracteres/3.Majuscules/majuscules.c
+++ Niveau 3/3.Gestion de caracteres/3.Majuscules/majuscules.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void    to_upper(char *str);
+void to_upper(char *str);
 
-int     main(void)
+int main(void)
 {
-    char    str[10001];
+    char str[10001];
 
     scanf("%[^\n]", str);
     to_upper(str);
     return (0);
 }
 
-void    to_upper(char *str)
+void to_upper(char *str)
 {
-    int     i;
+    int i;
 
     i = 0;
     while (str[i])
@@ -24,3 +24,33 @@ void    to_upper(char *str)
     }
     printf("%s\n", str);
 }
+
+/*
+#include <stdio.h>
+
+
+int     main(void)
+{
+    char    str[10001];
+    int     i;
+
+    scanf("%[^\n]", str);
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            printf("%c", str[i]);
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 'a';
+            str[i] += 'A';
+            printf("%c", str[i]);
+        }
+        else
+            printf("%c", str[i]);
+        i++;
+    }
+    return (0);
+}
+*/
