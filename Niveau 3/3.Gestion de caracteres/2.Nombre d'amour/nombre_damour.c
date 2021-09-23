3da1e5979c0a473aa4a6f0a02c7dc3b754cc6878
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-23 18:30:56 +0200

    update

--- Niveau 3/3.Gestion de caracteres/2.Nombre d'amour/nombre_damour.c
+++ Niveau 3/3.Gestion de caracteres/2.Nombre d'amour/nombre_damour.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int     convert_name(char *name);
-int     convert_i(int nb);
+int convert_name(char *name);
+int convert_i(int nb);
 
-int     main(void)
+int main(void)
 {
-    char    name_a[51];
-    char    name_b[51];
-    int     love_count_a;
-    int     love_count_b;
+    char name_a[51];
+    char name_b[51];
+    int love_count_a;
+    int love_count_b;
 
     scanf("%s %s", name_a, name_b);
     love_count_a = convert_name(name_a);
@@ -18,11 +18,11 @@ int     main(void)
     return (0);
 }
 
-int     convert_name(char *name)
+int convert_name(char *name)
 {
-    int     nb;
-    int     love;
-    int     i;
+    int nb;
+    int love;
+    int i;
 
     i = 0;
     love = 0;
@@ -90,10 +90,10 @@ int     convert_name(char *name)
         return (convert_i(love));
 }
 
-int     convert_i(int nb)
+int convert_i(int nb)
 {
-    int     digit;
-    int     tot;
+    int digit;
+    int tot;
 
     digit = 0;
     tot = 0;
@@ -107,3 +107,113 @@ int     convert_i(int nb)
     }
     return (convert_i(tot));
 }
+
+/*
+#include <stdio.h>
+
+int     split_nbr(int);
+int     convert(char*);
+
+int     main(void)
+{
+    char    name_1[51];
+    char    name_2[51];
+    int     count_1;
+    int     count_2;
+
+    scanf("%s %s", name_1, name_2);
+
+    count_1 = convert(name_1);
+    count_2 = convert(name_2);
+    if (count_1 < 10)
+        printf("%d ", count_1);
+    else
+    {
+        while (!(count_1 < 10))
+            count_1 =  split_nbr(count_1);
+        printf("%d ", count_1);
+    }
+    if (count_2 < 10)
+        printf("%d\n", count_2);
+    else
+    {
+        while (!(count_2 < 10))
+            count_2 =  split_nbr(count_2);
+        printf("%d\n", count_2);
+    }
+    return (0);
+}
+
+int     convert(char *str)
+{
+    char    alpf[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int     arr[26] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+    int     count;
+    int     i;
+    int     j;
+
+    count = 0;
+    i = 0;
+    while (str[i])
+    {
+        j = 0;
+        while (alpf[j] && j < (sizeof(arr) / sizeof(arr[0])))
+        {
+            if (str[i] == alpf[j])
+                count += arr[j];
+            j++;
+        }
+        i++;
+    }
+    return (count);
+}
+
+int     split_nbr(int nbr)
+{
+    int     mod;
+    int     res;
+
+    mod = 0;
+    res = 0;
+
+    while (nbr > 0)
+    {
+        mod = nbr % 10;
+        res += mod;
+        nbr /= 10;
+    }
+    return (res);
+}
+*/
+
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   int nombres[2];
+   for (int idPrenom = 0; idPrenom < 2; idPrenom = idPrenom + 1)
+   {
+      char prenom[51];
+      scanf("%s", prenom);
+      int nombre = 0;
+      int longueurPrenom = strlen(prenom);
+      for (int posLettre = 0; posLettre < longueurPrenom; posLettre = posLettre + 1)
+      {
+         nombre = nombre + (prenom[posLettre] - 'A');
+      }
+      while (nombre >= 10)
+      {
+         int sommeChiffre = 0;
+         while (nombre > 0)
+         {
+            sommeChiffre = sommeChiffre + nombre % 10;
+            nombre = nombre / 10;
+         }
+         nombre = sommeChiffre;
+      }
+      nombres[idPrenom] = nombre;
+   }
+   printf("%d %d", nombres[0], nombres[1]);
+}
+*/
