adf73793580e039cb3f0d31edd5107289f1ba35e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-29 17:21:11 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/1.Inversion d'une liste de livres/inversion_dune_liste_de_livres.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/1.Inversion d'une liste de livres/inversion_dune_liste_de_livres.c
@@ -2,6 +2,24 @@
 
 int     main(void)
 {
-    
+    int     tot_books;
+    int     i;
+
+    scanf("%d\n", &tot_books);
+
+    char    titles[tot_books][101];
+
+    i = 0;
+    while (i < tot_books)
+    {
+        scanf("%[^\n]%*c\n", titles[i]);
+        i++;
+    }
+
+    while (tot_books > 0)
+    {
+        printf("%s\n", titles[tot_books - 1]);
+        tot_books--;
+    }
     return (0);
 }
