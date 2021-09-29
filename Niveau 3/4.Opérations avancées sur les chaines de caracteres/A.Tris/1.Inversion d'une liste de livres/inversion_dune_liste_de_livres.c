8380d75f84ec17ec9bb86841ca104510c6b9a991
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-29 17:23:47 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/1.Inversion d'une liste de livres/inversion_dune_liste_de_livres.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/1.Inversion d'une liste de livres/inversion_dune_liste_de_livres.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int     main(void)
+int main(void)
 {
-    int     tot_books;
-    int     i;
+    int tot_books;
+    int i;
 
     scanf("%d\n", &tot_books);
 
-    char    titles[tot_books][101];
+    char titles[tot_books][101];
 
     i = 0;
     while (i < tot_books)
@@ -23,3 +23,23 @@ int     main(void)
     }
     return (0);
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+   int nbLivres;
+   scanf("%d\n", &nbLivres);
+   char titres[nbLivres][101];
+   for (int idLivre = 0; idLivre < nbLivres; idLivre = idLivre + 1)
+   {
+      scanf("%[^\n]\n", titres[idLivre]);
+   }
+   for (int idLivre = nbLivres - 1; idLivre >= 0; idLivre = idLivre - 1)
+   {
+      printf("%s\n", titres[idLivre]);
+   }
+}
+*/
