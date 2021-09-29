066b3f99234131d2acfbbf0919e87aa48744209b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-29 20:04:19 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/2.Trier des livres/trier_des_livres.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/2.Trier des livres/trier_des_livres.c
@@ -51,3 +51,28 @@ int     compare_text(const void *s1, const void *s2)
 {
     return (ft_strcmp((char*)(s1), (char*)(s2)));
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int compareTextes(const void* pTexte1, const void* pTexte2)
+{
+  return strcmp((char*)(pTexte1), (char*)(pTexte2) );
+}
+int main()
+{
+   int nbLivres;
+   scanf("%d\n", &nbLivres);
+   char titres[nbLivres][101];
+   for (int idLivre = 0; idLivre < nbLivres; idLivre = idLivre + 1)
+   {
+      scanf("%[^\n]\n", titres[idLivre]);
+   }
+   qsort(titres, nbLivres, 101 * sizeof(char), compareTextes);
+   for (int idLivre = 0; idLivre < nbLivres; idLivre = idLivre + 1)
+   {
+      printf("%s\n", titres[idLivre]);
+   }
+}
+ */
