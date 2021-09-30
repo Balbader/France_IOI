e584efadbcc0933740ffe7ebb5baaa0283da24ea
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-30 16:53:02 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/3.Lire ou ne pas lire/lire_ou_ne_pas_lire.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/3.Lire ou ne pas lire/lire_ou_ne_pas_lire.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int     ft_strcmp(char*, char*);
 
 int     main(void)
 {
     int     tot_books;
-    scanf("%d\n", &tot_books);
-    char    titles[tot_books][101];
-    char    temp;
+    char    temp[101];
     int     i;
+    int     res;
+
+    scanf("%d\n", &tot_books);
+
+    char    titles[tot_books][101];
 
     i = 0;
     while (i < tot_books)
@@ -15,26 +22,31 @@ int     main(void)
         i++;
     }
 
-    if (titles[0][0] == 'A')
-    {
-        printf("%s\n", titles[0]);
-        i = 1;
-        temp = titles[0][0];
-    }
-    else
-    {
-        temp = 'A';
-        i = 0;
-    }
-
+    strcpy(temp, titles[0]);
+    printf("%s\n", temp);
+    res = 0;
+    i = 1;
     while (i < tot_books)
     {
-        if (titles[i][0] > temp)
+        res = ft_strcmp(temp, titles[i]);
+        if (res < 0)
         {
             printf("%s\n", titles[i]);
-            temp = titles[i][0];
+            strcpy(temp, titles[i]);
+            i++;
         }
         i++;
     }
     return (0);
 }
+
+
+int     ft_strcmp(char *s1, char *s2)
+{
+    int     i;
+    i = 0;
+    while (s1[i] == s2[i] && s1[i] && s2[i])
+        i++;
+    return (s1[i] - s2[i]);
+}
+
