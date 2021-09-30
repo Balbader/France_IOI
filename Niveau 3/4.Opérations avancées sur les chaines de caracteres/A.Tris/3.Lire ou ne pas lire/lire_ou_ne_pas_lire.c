3f4c8b00783fb20370aff1a8b770b9c788853a47
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-30 11:26:01 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/3.Lire ou ne pas lire/lire_ou_ne_pas_lire.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/3.Lire ou ne pas lire/lire_ou_ne_pas_lire.c
@@ -2,6 +2,39 @@
 
 int     main(void)
 {
+    int     tot_books;
+    scanf("%d\n", &tot_books);
+    char    titles[tot_books][101];
+    char    temp;
+    int     i;
 
+    i = 0;
+    while (i < tot_books)
+    {
+        scanf("%[^\n]%*c\n", titles[i]);
+        i++;
+    }
+
+    if (titles[0][0] == 'A')
+    {
+        printf("%s\n", titles[0]);
+        i = 1;
+        temp = titles[0][0];
+    }
+    else
+    {
+        temp = 'A';
+        i = 0;
+    }
+
+    while (i < tot_books)
+    {
+        if (titles[i][0] > temp)
+        {
+            printf("%s\n", titles[i]);
+            temp = titles[i][0];
+        }
+        i++;
+    }
     return (0);
 }
