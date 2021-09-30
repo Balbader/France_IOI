e79f6e0618983cdd25c9e6a0dcc2383fc8a90772
Balbader <15971200+Balbader@users.noreply.github.com>
2021-09-30 15:42:35 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversion_de_dictionnaire.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversion_de_dictionnaire.c
@@ -2,21 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-int     ft_strcmp(char*, char*);
-int     compare_words(const void*, const void*);
+int ft_strcmp(char *, char *);
+int compare_words(const void *, const void *);
 
-int     main(void)
+int main(void)
 {
-    int     nbr_words;
-    char    word_1[51];
-    char    word_2[51];
-    char    arr[1001][256];
-    int     i;
+    int nbr_words;
+    char word_1[51];
+    char word_2[51];
+    char arr[1001][256];
+    int i;
 
     scanf("%d", &nbr_words);
 
-    char    temp_word_1[nbr_words][51];
-    char    temp_word_2[nbr_words][51];
+    char temp_word_1[nbr_words][51];
+    char temp_word_2[nbr_words][51];
 
     i = 0;
     while (i < nbr_words)
@@ -42,16 +42,46 @@ int     main(void)
     return (0);
 }
 
-int     ft_strcmp(char *s1, char *s2)
+int ft_strcmp(char *s1, char *s2)
 {
-    int     i;
+    int i;
     i = 0;
     while (s1[i] == s2[i] && s1[i] && s2[i])
         i++;
     return (s1[i] - s2[i]);
 }
 
-int     compare_words(const void *s1, const void *s2)
+int compare_words(const void *s1, const void *s2)
 {
-    return (ft_strcmp((char*)(s1), (char*)(s2)));
+    return (ft_strcmp((char *)(s1), (char *)(s2)));
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int compareTextes(const void* pTexte1, const void* pTexte2)
+{
+  return strcmp((char*)(pTexte1), (char*)(pTexte2) );
+}
+int main()
+{
+   int nbMots;
+   scanf("%d\n", &nbMots);
+   // Deux mots de taille 50 + l’espace + 1
+   char couplesMots[nbMots][102];
+   for (int idCouple = 0; idCouple < nbMots; idCouple = idCouple + 1)
+   {
+      char premier[51], second[51];
+      scanf("%s%s", premier, second);
+      strcpy(couplesMots[idCouple], second);
+      strcat(couplesMots[idCouple], " ");
+      strcat(couplesMots[idCouple], premier);
+   }
+   qsort(couplesMots, nbMots, 102 * sizeof(char), compareTextes);
+   for (int idCouple = 0; idCouple < nbMots; idCouple = idCouple + 1)
+   {
+      printf("%s\n", couplesMots[idCouple]);
+   }
+}
+*/
