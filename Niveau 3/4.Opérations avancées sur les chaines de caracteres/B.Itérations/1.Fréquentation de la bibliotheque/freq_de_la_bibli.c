22d318f94cdb504ec97f4cebd0fa360f77f8fdc9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-01 12:31:55 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/1.Fréquentation de la bibliotheque/freq_de_la_bibli.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/1.Fréquentation de la bibliotheque/freq_de_la_bibli.c
@@ -2,5 +2,14 @@
 
 int     main(void)
 {
+    int     nbr;
+    int     count;
+
+    count = 0;
+    while (scanf("%d", &nbr) != EOF)
+    {
+        count += nbr;
+    }
+    printf("%d\n", count);
     return (0);
 }
