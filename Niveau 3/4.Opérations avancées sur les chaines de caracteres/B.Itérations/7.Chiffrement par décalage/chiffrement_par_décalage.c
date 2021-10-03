cea0acc3bf6478220a4132cc103b51b25433798a
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-03 20:28:14 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
 int     main(void)
 {
     char    maj_alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     char    min_alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
-    char    str[1001] = "Ikio kyz rg ykiutjk vgmk ja robxk";
+    char    str[1001] = "ikio kyz rg ykiutjk vgmk ja robxk";
+    char    temp[1001];
+    int     len_min;
+    int     len_maj;
+    int     res;
     int     i;
     int     j;
 
+    strcpy(temp, str);
+
+    len_maj = strlen(maj_alphabet);
+    len_min = strlen(min_alphabet);
+    printf("len_min: %d\n", len_min);
+    res = 0;
     i = 0;
-    while (str[i])
+    while (str[i] && temp[i])
     {
-        if (str[i] == ' ')
+        j = 0;
+        while (min_alphabet[j])
         {
-            printf("%c", str[i]);
-            i++;
-        }
-
-        if (str[i] >= 'A' && str[i] <= 'Z')
-        {
-            j = 0;
-            while (maj_alphabet[j])
+            if (min_alphabet[j] == str[i])
             {
-                if (maj_alphabet[j] == str[i])
-                    printf("%c", maj_alphabet[j] - 6);
-                j++;
+                printf("letter before: %c\n", temp[i]);
+                res = (((j % len_min) + len_min) % len_min);
+                printf("res: %d\n", res);
+                temp[i] = min_alphabet[res];
+                printf("letter before: %c\n", temp[res]);
+                printf("\n");
             }
-        }
-
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            j = 0;
-            while (min_alphabet[j])
-            {
-                if (min_alphabet[j] == str[i])
-                    printf("%c", min_alphabet[i] - 6);
-                j++;
-            }
+            j++;
         }
         i++;
     }
+    printf("str : %s\n", str);
+    printf("temp: %s\n", temp);
     return (0);
 }
