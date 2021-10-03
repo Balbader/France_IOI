6836c41d84983ac4c3dd86cd9c7af0603d2db55e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-03 12:28:27 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/5.Consonnes/consonnes.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/5.Consonnes/consonnes.c
@@ -2,5 +2,24 @@
 
 int     main(void)
 {
+    char    voy[6] = "aeiouy";
+    char    letter;
+    int     i;
+
+    letter = 'b';
+    while (letter < 'z' + 1)
+    {
+        i = 0;
+        while (voy[i])
+        {
+            if(letter == voy[i])
+                letter++;
+            i++;
+        }
+        printf("%c", letter);
+        if (letter != 'z')
+            printf(" ");
+        letter++;
+    }
     return (0);
 }
