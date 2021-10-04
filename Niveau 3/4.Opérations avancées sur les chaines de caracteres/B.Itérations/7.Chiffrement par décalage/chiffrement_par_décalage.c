bb69ba0d3d338630b95255eb64c26671e1e6925e
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-04 19:28:43 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
@@ -5,13 +5,42 @@ void    cesar(char *str, int shift);
 
 int     main(void)
 {
-    char    str[1001] = "ikio kyz rg ykiutjk vgmk ja robxk";
-    char    temp[1001];
+    char    str[1001];
+    char    temp[1001][1001];
+    int     tot_pages;
+    int     i;
+    int     pages;
 
-    strcpy(temp, str);
 
-    printf("%s\n", str);
-    cesar(temp, -6);
+    scanf("%d\n", &tot_pages);
+
+    i = 0;
+    while (i < tot_pages - 1 )
+    {
+        scanf("%[^\n]\n", str);
+        strcpy(temp[i], str);
+        i++;
+    }
+
+    i = 0;
+    pages = i + 2;
+    while (i < tot_pages - 1)
+    {
+        if (pages % 2 == 0)
+        {
+            pages *= 3;
+            pages *= -1;
+            cesar(temp[i], pages);
+        }
+        else
+        {
+            pages *= -5;
+            pages *= -1;
+            cesar(temp[i], pages);
+        }
+        i++;
+        pages = i + 2;
+    }
     return (0);
 }
 
@@ -23,6 +52,22 @@ void    cesar(char *str, int shift)
     i = 0;
     while (str[i] != '\0')
     {
+        if (str[i] != ((str[i] >= 'A' && str[i] <= 'Z')
+                    || (str[i] >= 'a' && str[i] <= 'z')))
+        {
+            str[i] = str[i];
+        }
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            c = str[i] - 'A';
+            c += shift;
+            while (c < 0)
+                c += 26;
+            while (c > 25)
+                c -= 26;
+            str[i] = c + 'A';
+        }
+
         if (str[i] >= 'a' && str[i] <= 'z')
         {
             c = str[i] - 'a'; // 0 <= c < 26 i.e. initial position
