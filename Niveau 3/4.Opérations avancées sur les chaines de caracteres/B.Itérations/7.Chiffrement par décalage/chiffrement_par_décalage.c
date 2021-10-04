26bedd41eeb13006e2719b29c6e9cf3cee231ce1
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-04 16:07:08 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/7.Chiffrement par décalage/chiffrement_par_décalage.c
@@ -3,36 +3,25 @@
 
 int     main(void)
 {
-    char    maj_alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char    min_alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+    char    alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
     char    str[1001] = "ikio kyz rg ykiutjk vgmk ja robxk";
     char    temp[1001];
-    int     len_min;
-    int     len_maj;
     int     res;
     int     i;
     int     j;
 
     strcpy(temp, str);
 
-    len_maj = strlen(maj_alphabet);
-    len_min = strlen(min_alphabet);
-    printf("len_min: %d\n", len_min);
     res = 0;
     i = 0;
     while (str[i] && temp[i])
     {
         j = 0;
-        while (min_alphabet[j])
+        while (alphabet[j])
         {
-            if (min_alphabet[j] == str[i])
+            if (alphabet[j] == str[i])
             {
-                printf("letter before: %c\n", temp[i]);
-                res = (((j % len_min) + len_min) % len_min);
-                printf("res: %d\n", res);
-                temp[i] = min_alphabet[res];
-                printf("letter before: %c\n", temp[res]);
-                printf("\n");
+                temp[i] = alphabet[j - 6];
             }
             j++;
         }
