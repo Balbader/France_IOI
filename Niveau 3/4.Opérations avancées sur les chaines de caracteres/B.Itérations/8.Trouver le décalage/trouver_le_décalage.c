1d8b92d1b56cbcbcf57b5c78a1ef84395abfcefe
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-08 16:58:45 +0200

    update

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/8.Trouver le décalage/trouver_le_décalage.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/8.Trouver le décalage/trouver_le_décalage.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
-int     main(void)
+void	cesar(char *str, int shift);
+
+int 	main(void)
 {
-	
     return (0);
+}
+
+void	cesar(char *str, int shift)
+{
+	int		c;
+	int		i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			c = str[i] - 'A';
+			c += shift;
+			while (c < 0)
+				c += 26;
+			while (c > 0)
+				c -= 26;
+			str[i] = c + 'A';
+		}
+
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			c = str[i] - 'a';
+			c += shift;
+			while (c < 0)
+				c += 26;
+			while (c > 0)
+				c -= 26;
+			str[i] = c + 'a';
+		}
+		i++;
+	}
+	printf("%s\n", str);
 }
