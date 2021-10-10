7afdc7b28b5f41684b87a69d47c31112131389d3
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-10 16:26:26 +0200

    work in progress

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/8.Trouver le décalage/trouver_le_décalage.c
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/B.Itérations/8.Trouver le décalage/trouver_le_décalage.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 void	cesar(char *str, int shift);
-void	to_lower(char *str);
-int		freq_counter(char *str);
+/*void	to_lower(char *str);*/
+/*int		freq_counter(char *str);*/
 
 int		main(void)
 {
 	char	str[10001] = "Np epiep fetwtdp fy opnlwlrp op zykp nlclnepcpd.";
-	char	new_str[10001];
-	int		res;
 	int		shift;
-	res = freq_counter(str);
-	printf ("res: %d\n", res);
 	shift = 11;
-	printf ("shift: %d\n", shift);
+	shift *= -1;
+	printf("%d\n", shift);
 	cesar(str, shift);
 	return (0);
 }
@@ -33,7 +30,7 @@ void	cesar(char *str, int shift)
 			c += shift;
 			while (c < 0)
 				c += 26;
-			while (c > 0)
+			while (c > 25)
 				c -= 26;
 			str[i] = c + 'A';
 		}
@@ -44,7 +41,7 @@ void	cesar(char *str, int shift)
 			c += shift;
 			while (c < 0)
 				c += 26;
-			while (c > 0)
+			while (c > 25)
 				c -= 26;
 			str[i] = c + 'a';
 		}
@@ -53,48 +50,48 @@ void	cesar(char *str, int shift)
 	printf("%s\n", str);
 }
 
-int		freq_counter(char *str)
-{
-	char	alph[27] = "abcdefghijklmnopqrstuvwxyz";
-	char	res;
-	int		arr[26] = {0};
-	int		count;
-	int		i;
-	int		j;
+/*int		freq_counter(char *str)*/
+/*{*/
+	/*char	alph[27] = "abcdefghijklmnopqrstuvwxyz";*/
+	/*char	res;*/
+	/*int		arr[26] = {0};*/
+	/*int		count;*/
+	/*int		i;*/
+	/*int		j;*/
 
-	count = 0;
-	i = 0;
-	while (str[i])
-	{
-		j = 0;
-		while (alph[j])
-		{
-			if (str[i] == alph[j])
-			{
-				arr[j] += 1;
-				if (arr[j] > count)
-				{
-					count = arr[j];
-					res	= alph[j];
-				}
-			}
-			j++;
-		}
-		i++;
-	}
-	return (count);
-}
+	/*count = 0;*/
+	/*i = 0;*/
+	/*while (str[i])*/
+	/*{*/
+		/*j = 0;*/
+		/*while (alph[j])*/
+		/*{*/
+			/*if (str[i] == alph[j])*/
+			/*{*/
+				/*arr[j] += 1;*/
+				/*if (arr[j] > count)*/
+				/*{*/
+					/*count = arr[j];*/
+					/*res	= alph[j];*/
+				/*}*/
+			/*}*/
+			/*j++;*/
+		/*}*/
+		/*i++;*/
+	/*}*/
+	/*return (count);*/
+/*}*/
 
-void	to_lower(char *str)
-{
-	int		i;
+/*void	to_lower(char *str)*/
+/*{*/
+	/*int		i;*/
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
-	}
-}
+	/*i = 0;*/
+	/*while (str[i])*/
+	/*{*/
+		/*if (str[i] >= 'A' && str[i] <= 'Z')*/
+			/*str[i] += 32;*/
+		/*i++;*/
+	/*}*/
+/*}*/
 
