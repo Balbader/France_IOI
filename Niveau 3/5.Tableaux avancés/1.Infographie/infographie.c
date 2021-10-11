2e4350ecd101a6085329c8977d71da85968c5308
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-11 13:39:16 +0200

    update

--- Niveau 3/5.Tableaux avancés/1.Infographie/infographie.c
+++ Niveau 3/5.Tableaux avancés/1.Infographie/infographie.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+/* 1 3 7 5 o
+ * 1 = ligne de depart
+ * 3 = point de depart du char a imprimer
+ * 7 = nombre de lignes a imprimer
+ * 5 = point d'arrivee du char a imprimer
+ */
+
+
 int		main(void)
 {
-	int		x; //nbr of lines
-	int		y; //nbr of col
+	int		x;			//nbr of lines
+	int		y;			//nbr of col
 	int		tot_rec;
-	int		x1; // top
-	int		y1; // left
-	int		x2; // bottom
-	int		y2; // right
-	int		i;
-	int		j;
+	int		line;		// top
+	int		start;		// left
+	int		tot_lines;	// bottom
+	int		end;		// right
 	char	color;
 	char	pixel = '.';
+	int		i;
+	int		j;
 
 	scanf("%d %d", &x, &y);
-	/*scanf("%d\n", &tot_rec);*/
+	scanf("%d\n", &tot_rec);
 
-	/*i = 0;*/
-	/*while (i < tot_rec)*/
-	/*{*/
-		/*scanf("%d %d %d %d %c\n", &x1, &y1, &x2, &y2, &color);*/
-	/*}*/
+	i = 0;
+	while (i < tot_rec)
+	{
+		scanf("%d %d %d %d %c\n", &line, &start, &tot_lines, &end, &color);
+		i++;
+	}
 
 	i = 0;
 	while (i < x)
