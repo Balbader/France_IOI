d7f4c0d3f32be30b827da5132d7d88a0c2cd6863
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-14 11:14:32 +0200

    update

--- Niveau 3/5.Tableaux avancés/1.Infographie/infographie.c
+++ Niveau 3/5.Tableaux avancés/1.Infographie/infographie.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-
-/* 1 3 7 5 o
- * 1 = ligne de depart
- * 3 = point de depart du char a imprimer
- * 7 = nombre de lignes a imprimer
- * 5 = point d'arrivee du char a imprimer
- */
+#include <string.h>
 
 int		main(void)
 {
@@ -18,32 +12,18 @@ int		main(void)
 	int		tot_lines;
 	int		end;
 
-	char	grid[num_rows][num_columns];
-	char	arr[num_rows];
-
-	char	default_letter;
+	char	default_letter = '.';
 	char	new_letter;
 
 	int		i;
 	int		j;
+	int		k;
 
 	scanf("%d %d\n", &num_rows, &num_columns);
 	scanf("%d\n", &tot_rectangles);
 
-	default_letter = '.';
-	i = 0;
-	while (i < tot_rectangles)
-	{
-		scanf("%d %d %d %d %c\n", &line, &start, &tot_lines, &end, &new_letter);
-
-		i++;
-	}
-
-
-
-
-
-
+	int 	grid[num_rows][num_columns];
+	
 
 	i = 0;
 	while (i < num_rows)
@@ -51,11 +31,44 @@ int		main(void)
 		j = 0;
 		while (j < num_columns)
 		{
-
+			grid[i][j] = default_letter;
 			j++;
 		}
 		i++;
 	}
+
+	k = 0;
+	while (k < tot_rectangles)
+	{
+		scanf("%d %d %d %d %c\n",
+				&line, &start, &tot_lines, &end, &new_letter);
+		i = 0;
+		while (i < num_rows)
+		{
+			j = 0;
+			while (j < num_columns)
+			{
+				if ((j >= start && j <= end)
+						&& (i >= line && i <= tot_lines))
+					grid[i][j] = new_letter;
+				j++;
+			}
+			i++;
+		}
+		k++;
+	}
+
+	i = 0;
+	while (i < num_rows)
+	{
+		j = 0;
+		while (j < num_columns)
+		{
+			printf("%c", grid[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
 	return (0);
 }
-
