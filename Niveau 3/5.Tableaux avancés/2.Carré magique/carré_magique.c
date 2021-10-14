24c166068f55ef9cf49d67ce60dfd0d6b4b7c2e1
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-14 14:08:04 +0200

    update

--- Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
+++ Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
@@ -2,5 +2,70 @@
 
 int	main(void)
 {
+	int		grid[20][20];
+	int		res[402];
+	int		nbr;
+	int		grid_nbr;
+	int		count;
+	int		diag_x;
+	int		diag_y;
+	int		i;
+	int		j;
+
+	scanf("%d\n", &nbr);
+	i = 0;
+	while (i < nbr)
+	{
+		j = 0;
+		while (j < nbr)
+		{
+			scanf("%d\n", &grid_nbr);
+			grid[i][j] = grid_nbr;
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < nbr)
+	{
+		j = 0;
+		while (j < nbr)
+		{
+			printf("[%d][%d]: %d ", i, j, grid[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	count = 0;
+	i = 0;
+	while (i < nbr)
+	{
+		j = 0;
+		while (j < nbr)
+		{
+			count += grid[i][j];
+			j++;
+		}
+		printf("ligne: %d - count: %d\n",i, count);
+		count = 0;
+		i++;
+	}
+
+	count = 0;
+	j = 0;
+	while (j < nbr)
+	{
+		i = 0;
+		while (i < nbr)
+		{
+			count += grid[i][j];
+			i++;
+		}
+		printf("col: %d - count: %d\n",j , count);
+		count = 0;
+		j++;
+	}
 	return (0);
 }
+
