e883126a8f19b1e45f0e96330ebcb0fdda44bead
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-15 11:53:23 +0200

    update

--- Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
+++ Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
@@ -4,12 +4,12 @@ int	counter_x(int nbr, int grid[20][20]);
 int	counter_y(int nbr, int grid[20][20]);
 int	diag_x(int nbr, int grid[20][20]);
 int diag_y(int nbr, int grid[20][20]);
+void check_res(int *grid);
 
 int	main(void)
 {
 	int		grid[20][20];
 	int		nbr;
-	int		grid_nbr;
 	int		i;
 	int		j;
 
@@ -20,16 +20,14 @@ int	main(void)
 		j = 0;
 		while (j < nbr)
 		{
-			scanf("%d\n", &grid_nbr);
-			grid[i][j] = grid_nbr;
+			scanf("%d\n", &grid[i][j]);
 			j++;
 		}
 		i++;
 	}
-	printf("counter_x: %d\n", counter_x(nbr, grid));
-	printf("counter_y: %d\n", counter_y(nbr, grid));
-	printf("diag_x: %d\n", diag_x(nbr, grid));
-	printf("diag_y: %d\n", diag_y(nbr, grid));
+
+	int		res[20] = {counter_x(nbr, grid), counter_y(nbr, grid), diag_x(nbr, grid), diag_y(nbr, grid)};
+	check_res(res);
 	return (0);
 }
 
@@ -126,4 +124,27 @@ int	diag_y(int nbr, int grid[20][20])
 		j--;
 	}
 	return (count);
+}
+
+void check_res(int *grid)
+{
+	int count;
+	int		i;
+
+	count = 0;
+	i = 0;
+	while (i < (sizeof(*grid) / sizeof(grid[0])))
+	{
+		if (grid[i] == grid[i+1])
+		{
+			count++;
+		}
+		i++;
+	}
+
+	if (count == (sizeof(*grid) / sizeof(grid[0])))
+		printf("yes\n");
+	else
+		printf("no\n");
+
 }
