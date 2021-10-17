3577d7a5967a3ed2fcd12f39152ae410d24f4f06
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-17 17:02:06 +0200

    wip

--- Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
+++ Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
@@ -12,14 +12,14 @@ int		main(void)
 	char	arr[8][8];
 	int		knight_x;
 	int		knight_y;
-	int		temp;
 	int		i;
 	int		j;
 
 	scan_arr(arr);
 
 	// find knight positions
-	temp = 0;
+	knight_x = 0;
+	knight_y = 0;
 	i = 0;
 	while (i < 8)
 	{
@@ -30,8 +30,7 @@ int		main(void)
 			{
 				knight_x = i;
 				knight_y = j;
-				temp = check_moves(knight_x, knight_y, arr);
-				if (temp == true)
+				if (check_moves(knight_x, knight_y, arr) == true)
 				{
 					printf("yes\n");
 					return (0);
@@ -41,7 +40,7 @@ int		main(void)
 		}
 		i++;
 	}
-	if (temp == false)
+	if (check_moves(knight_x, knight_y, arr) == false)
 	{
 		printf("no\n");
 		return (0);
@@ -76,15 +75,15 @@ bool	check_moves(int knight_x, int knight_y, char arr[8][8])
 	int		y_move[8] = {1, 2, 2, 1, -1, -2, -2, -1};
 	int		i;
 	int		j;
-	int		count;
 
-	count = 0;
 	i = 0;
 	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
 		{
+			if (knight_x < 0 || knight_x > 8 || knight_y < 0 || knight_y > 8)
+				return ' ';
 			if (arr[knight_x + x_move[i]][knight_y + y_move[j]] >= 'a'
 					&& arr[knight_x + x_move[i]][knight_y + y_move[j]] <= 'z')
 				return (true);
@@ -94,4 +93,3 @@ bool	check_moves(int knight_x, int knight_y, char arr[8][8])
 	}
 	return (false);
 }
-
