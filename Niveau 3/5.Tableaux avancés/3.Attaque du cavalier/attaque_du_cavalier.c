344731d69239b72e20a9617e7f638d49124b261f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-17 15:41:06 +0200

    update

--- Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
+++ Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 
-typedef int bool;
-#define true 1
-#define false
-
 char	scan_arr(char arr[8][8]);
 
 int		main(void)
 {
 	char	arr[8][8];
+	int		knight_pos[8][8];
 	int		i;
 	int		j;
 
 	scan_arr(arr);
+
+	// find knight positions
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (arr[i][j] == 'C')
+			{
+				knight_pos[i][j] = arr[i][j];
+				printf("knight_pos[%d][%d]\n", i, j);
+			}
+			j++;
+		}
+		i++;
+	}
+
 	return (0);
 }
 
@@ -35,4 +50,3 @@ char	scan_arr(char arr[8][8])
 	}
 	return (**arr);
 }
-
