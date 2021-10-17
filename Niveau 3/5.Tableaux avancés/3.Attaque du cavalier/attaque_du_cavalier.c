a7ed22667711e9e6bd2986e89b5178bfae6abae3
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-17 16:32:58 +0200

    udpate

--- Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
+++ Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 char	scan_arr(char arr[8][8]);
-
-/*void	print_arr(int arr[8]);*/
+int		check_moves(int knight_x, int knight_y, char arr[8][8]);
+void	check_count(int nbr);
 
 int		main(void)
 {
 	char	arr[8][8];
-	int		knight_pos[8][8];
-	int		knight_x[8];
-	int		knight_y[8];
-	int		x_move[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-	int		y_move[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+	int		knight_x;
+	int		knight_y;
 	int		i;
 	int		j;
 
@@ -26,10 +23,9 @@ int		main(void)
 		{
 			if (arr[i][j] == 'C')
 			{
-				knight_pos[i][j] = arr[i][j];
-				knight_x[i] = i;
-				knight_y[j] = j;
-				printf("knight_pos[%d][%d]\n", knight_x[i], knight_y[j]);
+				knight_x = i;
+				knight_y = j;
+				check_count(check_moves(knight_x, knight_y, arr));
 			}
 			j++;
 		}
@@ -58,14 +54,43 @@ char	scan_arr(char arr[8][8])
 	return (**arr);
 }
 
-/*void	print_arr(int arr[8])*/
-/*{*/
-	/*int		i;*/
+int		check_moves(int knight_x, int knight_y, char arr[8][8])
+{
 
-	/*i = 0;*/
-	/*while (i < 8)*/
-	/*{*/
-		/*printf("%d ", arr[i]);*/
-		/*i++;*/
-	/*}*/
-/*}*/
+	int		x_move[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+	int		y_move[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+	int		i;
+	int		j;
+	int		count;
+
+	count = 0;
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (arr[knight_x + x_move[i]][knight_y + y_move[j]] >= 'a'
+					&& arr[knight_x + x_move[i]][knight_y + y_move[j]] <= 'z')
+				count += 1;
+			j++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+void	check_count(int nbr)
+{
+	if (nbr > 0)
+	{
+		printf("yes\n");
+		printf("%d\n", nbr);
+		nbr = 0;
+	}
+	if (nbr == 0)
+	{
+		printf("no\n");
+		printf("%d\n", nbr);
+	}
+}
