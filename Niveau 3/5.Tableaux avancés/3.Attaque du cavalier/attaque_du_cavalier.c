b9069012bca1675ca0fab2fef93f4ae5fbe98ded
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-19 13:22:09 +0200

    update

--- Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
+++ Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.c
@@ -9,75 +9,81 @@ t_bool	check_moves(int knight_x, int knight_y, char arr[8][8]);
 
 int	main(void)
 {
-   char	arr[8][8];
-   int	i;
-   int	j;
+	char	arr[8][8];
+	int	i;
+	int	j;
 
-   scan_arr(arr);
-   i = 0;
-   while (i < 8)
-   {
-      j = 0;
-      while (j < 8)
-      {
-         if (arr[i][j] == 'C')
-         {
-            if (check_moves(i, j, arr) == TRUE)
-            {
-               printf("yes\n");
-               return (0);
-            }
-            else
-            {
-               printf("no\n");
-               return (0);
-            }
-         }
-         j++;
-      }
-      i++;
-   }
-   return (0);
+	scan_arr(arr);
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (arr[i][j] == 'C')
+			{
+				if (check_moves(i, j, arr) == FALSE)
+				{
+					j++;
+					i++;
+				}
+				if (check_moves(i, j, arr) == TRUE)
+				{
+					printf("yes\n");
+					return (0);
+				}
+				else
+				{
+					printf("no\n");
+					return (0);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
 char  scan_arr(char arr[8][8])
 {
-   int      i;
-   int      j;
+	int      i;
+	int      j;
 
-   i = 0;
-   while (i < 8)
-   {
-      j = 0;
-      while (j < 8)
-      {
-         scanf("%c", &arr[i][j]);
-         j++;
-      }
-      scanf("\n");
-      i++;
-   }
-   return (**arr);
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			scanf("%c", &arr[i][j]);
+			j++;
+		}
+		scanf("\n");
+		i++;
+	}
+	return (**arr);
 }
 
 t_bool   check_moves(int knight_x, int knight_y, char arr[8][8])
 {
-   int      x_move[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-   int      y_move[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-   int      i;
-   int      j;
+	int      x_move[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+	int      y_move[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+	int      i;
+	int      j;
 
-   j = 0;
-   i = 0;
-   while (i < 8 && j < 8)
-   {
-      if (arr[knight_x + x_move[i]][knight_y + y_move[j]] >= 'a'
-            && arr[knight_x + x_move[i]][knight_y + y_move[j]] <= 'z')
-         return (TRUE);
-      j++;
-      i++;
-   }
-   return (FALSE);
+	j = 0;
+	i = 0;
+	while (i < 8 && j < 8)
+	{
+		if (arr[knight_x + x_move[i]][knight_y + y_move[j]] < 8
+				|| arr[knight_x + x_move[i]][knight_y + y_move[j]] > 8)
+			return (' ');
+		if (arr[knight_x + x_move[i]][knight_y + y_move[j]] >= 'a'
+				&& arr[knight_x + x_move[i]][knight_y + y_move[j]] <= 'z')
+			return (TRUE);
+		j++;
+		i++;
+	}
+	return (FALSE);
 }
-
-
