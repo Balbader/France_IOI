ecfa7e12c5068be8f439fc286f46a598fb1bab52
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-22 16:43:34 +0200

    update

--- Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
+++ Niveau 3/5.Tableaux avancés/2.Carré magique/carré_magique.c
@@ -1,186 +1,162 @@
 #include <stdio.h>
 
-int		counter_x(int nbr, int grid[20][20]);
-int		counter_y(int nbr, int grid[20][20]);
-int		diag_x(int nbr, int grid[20][20]);
-int		diag_y(int nbr, int grid[20][20]);
-void	check_res(int nbr, int *grid);
-void	check_grid(int nbr, int grid[20][20]);
+int      counter_x(int nbr, int grid[20][20]);
+int      counter_y(int nbr, int grid[20][20]);
+int      diag_x(int nbr, int grid[20][20]);
+int      diag_y(int nbr, int grid[20][20]);
+void  check_res(int nbr, int *grid);
 
-int	main(void)
+int   main(void)
 {
-	int		grid[20][20];
-	int		res[1001];
-	int		nbr;
-	int		i;
-	int		j;
+   int      grid[20][20];
+   int      res[1001];
+   int      nbr;
+   int      i;
+   int      j;
 
-	scanf("%d\n", &nbr);
+   scanf("%d\n", &nbr);
 
-	i = 0;
-	while (i < nbr)
-	{
-		j = 0;
-		while (j < nbr)
-		{
-			scanf("%d\n", &grid[i][j]);
-			j++;
-		}
-		i++;
-	}
-	/*check_grid(nbr, grid);*/
-
-	i = 0;
-	while (i <= (nbr * 2) + 2)
-	{
-		if (i <= nbr)
-			res[i] = counter_x(nbr, grid);
-		else if (i > nbr && i <= (nbr * 2))
-			res[i] = counter_y(nbr, grid);
-		else if (i > (nbr * 2) && i < (nbr * 2) + 2)
-			res[i] = diag_x(nbr, grid);
-		else
-			res[i] = diag_y(nbr, grid);
-		i++;
-	}
-	check_res(nbr, res);
-	return (0);
+   i = 0;
+   while (i < nbr)
+   {
+      j = 0;
+      while (j < nbr)
+      {
+         scanf("%d\n", &grid[i][j]);
+         j++;
+      }
+      i++;
+   }
+   i = 0;
+   while (i <= (nbr * 2) + 2)
+   {
+      if (i <= nbr)
+         res[i] = counter_x(nbr, grid);
+      else if (i > nbr && i <= (nbr * 2))
+         res[i] = counter_y(nbr, grid);
+      else if (i > (nbr * 2) && i < (nbr * 2) + 2)
+         res[i] = diag_x(nbr, grid);
+      else
+         res[i] = diag_y(nbr, grid);
+      i++;
+   }
+   check_res(nbr, res);
+   return (0);
 }
 
-void	check_grid(int nbr, int grid[nbr][nbr])
+void  check_res(int nbr, int *grid)
 {
-	int		row;
-	int		col;
+   int      temp;
+   int      count;
+   int      nb;
+   int      i;
 
-	row = 0;
-	while (row < nbr)
-	{
-		col = 0;
-		while (col < nbr)
-		{
-			if (grid[row][col] == grid[row][col] + 1)
-			{
-				printf("no\n");
-				break;
-			}
-			col++;
-		}
-		row++;
-	}
-}
-
-void	check_res(int nbr, int *grid)
-{
-	int		temp;
-	int		count;
-	int		nb;
-	int		i;
-
-	nb = (nbr * 2) + 2;
-	count = 0;
-	temp = grid[0];
-	i = 0;
-	while (i < nb)
-	{
-		if (grid[i] == temp)
-			count++;
-		i++;
-	}
-	if (count == nb)
-		printf("yes\n");
-	else
-		printf("no\n");
+   nb = (nbr * 2) + 2;
+   count = 0;
+   temp = grid[0];
+   i = 0;
+   while (i < nb)
+   {
+      if (grid[i] == temp)
+         count++;
+      i++;
+   }
+   if (count == nb)
+      printf("yes\n");
+   else
+      printf("no\n");
 
 }
 
-int	counter_x(int nbr, int grid[nbr][nbr])
+int   counter_x(int nbr, int grid[20][20])
 {
-	int		count;
-	int		temp;
-	int		i;
-	int		j;
+   int      count;
+   int      temp;
+   int      i;
+   int      j;
 
-	temp = 0;
-	i = 0;
-	while (i < nbr)
-	{
-		j = 0;
-		while (j < nbr)
-		{
-			count += grid[i][j];
-			temp = count;
-			j++;
-		}
-		count = 0;
-		i++;
-	}
-	return (temp);
+   temp = 0;
+   i = 0;
+   while (i < nbr)
+   {
+      j = 0;
+      while (j < nbr)
+      {
+         count += grid[i][j];
+         temp = count;
+         j++;
+      }
+      count = 0;
+      i++;
+   }
+   return (temp);
 }
 
-int	counter_y(int nbr, int grid[nbr][nbr])
+int   counter_y(int nbr, int grid[20][20])
 {
-	int		count;
-	int		temp;
-	int		i;
-	int		j;
+   int      count;
+   int      temp;
+   int      i;
+   int      j;
 
-	temp = 0;
-	j = 0;
-	while (j < nbr)
-	{
-		i = 0;
-		while (i < nbr)
-		{
-			count += grid[i][j];
-			temp = count;
-			i++;
-		}
-		count = 0;
-		j++;
-	}
-	return (temp);
+   temp = 0;
+   j = 0;
+   while (j < nbr)
+   {
+      i = 0;
+      while (i < nbr)
+      {
+         count += grid[i][j];
+         temp = count;
+         i++;
+      }
+      count = 0;
+      j++;
+   }
+   return (temp);
 }
 
-int	diag_x(int nbr, int grid[nbr][nbr])
+int   diag_x(int nbr, int grid[20][20])
 {
-	int		count;
-	int		i;
-	int		j;
+   int      count;
+   int      i;
+   int      j;
 
-	count = 0;
-	i = 0;
-	while (i < nbr)
-	{
-		j = 0;
-		while (j < nbr)
-		{
-			if (i == j)
-				count += grid[i][j];
-			j++;
-		}
-		i++;
-	}
-	return (count);
+   count = 0;
+   i = 0;
+   while (i < nbr)
+   {
+      j = 0;
+      while (j < nbr)
+      {
+         if (i == j)
+            count += grid[i][j];
+         j++;
+      }
+      i++;
+   }
+   return (count);
 }
 
-int	diag_y(int nbr, int grid[nbr][nbr])
+int   diag_y(int nbr, int grid[20][20])
 {
-	int		count;
-	int		i;
-	int		j;
+   int      count;
+   int      i;
+   int      j;
 
-	count = 0;
-	j = nbr;
-	while (j >= 0)
-	{
-		i = nbr;
-		while (i >= 0)
-		{
-			if (i == j)
-				count += grid[i][j];
-			i--;
-		}
-		j--;
-	}
-	return (count);
+   count = 0;
+   j = nbr;
+   while (j >= 0)
+   {
+      i = nbr;
+      while (i >= 0)
+      {
+         if (i == j)
+            count += grid[i][j];
+         i--;
+      }
+      j--;
+   }
+   return (count);
 }
+
