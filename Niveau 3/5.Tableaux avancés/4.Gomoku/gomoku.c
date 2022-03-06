90defa0389c6ec8ec78eec9bec3dbcd40a891ee8
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-06 21:03:36 +0100

    gomoku.c

--- Niveau 3/5.Tableaux avancés/4.Gomoku/gomoku.c
+++ Niveau 3/5.Tableaux avancés/4.Gomoku/gomoku.c
@@ -4,61 +4,53 @@ typedef int t_bool;
 #define TRUE 1
 #define FALSE 0
 
-void	horizontal_win(int nbr, int arr[40][40]);
+void horizontal_win(int nbr, int arr[40][40]);
 
-int main(void)
-{
-	int		nbr;
-	int		arr[40][40];
-	int		count;
-	int		i;
-	int		j;
+int main(void) {
+  int nbr;
+  int arr[40][40];
+  int count;
+  int i;
+  int j;
 
-	scanf("%d\n", &nbr);
-	i = 0;
-	while (i < nbr)
-	{
-		j = 0;
-		while (j < nbr)
-		{
-			scanf("%d", &arr[i][j]);
-			j++;
-		}
-		i++;
-	}
+  scanf("%d\n", &nbr);
+  i = 0;
+  while (i < nbr) {
+    j = 0;
+    while (j < nbr) {
+      scanf("%d", &arr[i][j]);
+      j++;
+    }
+    i++;
+  }
 
-	horizontal_win(nbr, arr);
-	return (0);
+  horizontal_win(nbr, arr);
+  return (0);
 }
 
-void	horizontal_win(int nbr, int arr[40][40])
-{
-	int		i;
-	int		j;
-	int		count;
-	int		temp;
+void horizontal_win(int nbr, int arr[40][40]) {
+  int i;
+  int j;
+  int count;
+  int temp;
 
-	count = 0;
-	temp = 0;
-	i = 0;
-	while (i < nbr)
-	{
-		j = 0;
-		while (j < nbr)
-		{
-			if (arr[i][j] == arr[i][j + 1])
-			{
-				count++;
-				temp = arr[i][j];
-				if (count == nbr)
-					printf("%d\n", temp);
-			}
-			else
-				count = 0;
-			j++;
-		}
-		i++;
-	}
-	if (count != nbr)
-		printf("FALSE\n");
+  count = 0;
+  temp = 0;
+  i = 0;
+  while (i < nbr) {
+    j = 0;
+    while (j < nbr) {
+      if (arr[i][j] == arr[i][j + 1]) {
+        count++;
+        temp = arr[i][j];
+        if (count == nbr)
+          printf("%d\n", temp);
+      } else
+        count = 0;
+      j++;
+    }
+    i++;
+  }
+  if (count != nbr)
+    printf("FALSE\n");
 }
