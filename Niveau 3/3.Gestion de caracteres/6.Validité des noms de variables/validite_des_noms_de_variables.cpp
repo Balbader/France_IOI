9b59876297d6896559743115f1d1fddedf0703a5
balbader <basilalbapro@gmail.com>
2024-01-10 21:56:38 +0100

    WIP

--- Niveau 3/3.Gestion de caracteres/6.Validité des noms de variables/validite_des_noms_de_variables.cpp
+++ Niveau 3/3.Gestion de caracteres/6.Validité des noms de variables/validite_des_noms_de_variables.cpp
@@ -6,10 +6,29 @@
 #include <cstdlib>
 #include <vector>
 
+// checks if all the characters are ok
+bool firstCheck(std::string str) {
+	for (int i = 0; i < str.length(); ++i) {
+		if (! ((str[i] != '_') || (isalpha(str[i])) || (str[i] >= '0' && str[i] <= '9')))
+			return 1;
+	}
+	return 0;
+}
+
 int main(void) {
 	int nb;
 	std::cin >> nb;
 	std::cin.ignore();
+
+	std::vector<std::string> titles;
+	std::vector<std::string> titlesBackwards(nb);
+
+	for (int i = 0; i < nb; ++i) {
+		std::string tmp;
+		std::string title;
+		std::getline(std::cin, title);
+		titles.push_back(title);
+	}
 
 	return 0;
 }
