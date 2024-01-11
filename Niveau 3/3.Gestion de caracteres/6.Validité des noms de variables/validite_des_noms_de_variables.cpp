54700dd435fdfff29020bd6ead101b519f3c24b0
balbader <basilalbapro@gmail.com>
2024-01-11 06:28:30 +0100

    WIP

--- Niveau 3/3.Gestion de caracteres/6.Validité des noms de variables/validite_des_noms_de_variables.cpp
+++ Niveau 3/3.Gestion de caracteres/6.Validité des noms de variables/validite_des_noms_de_variables.cpp
@@ -7,11 +7,27 @@
 #include <vector>
 
 // checks if all the characters are ok
-bool firstCheck(std::string str) {
-	for (int i = 0; i < str.length(); ++i) {
-		if (! ((str[i] != '_') || (isalpha(str[i])) || (str[i] >= '0' && str[i] <= '9')))
+bool checkFirstChar(std::string str) {
+
+	if (!(str[0] == '_' || isalpha(str[0])))
+		return 1;
+	else
+		return 0;
+}
+
+// checks if the remaining characters are ok
+bool checkStr(std::string str) {
+
+	int i = 1;
+
+	while (str[i]) {
+		if ((str[i] == '_' || isalpha(str[i]) ||  isalnum(str[i])))
+			++i;
+		else
 			return 1;
+		++i;
 	}
+
 	return 0;
 }
 
@@ -21,13 +37,21 @@ int main(void) {
 	std::cin.ignore();
 
 	std::vector<std::string> titles;
-	std::vector<std::string> titlesBackwards(nb);
 
 	for (int i = 0; i < nb; ++i) {
-		std::string tmp;
 		std::string title;
 		std::getline(std::cin, title);
 		titles.push_back(title);
+	}
+
+	for (int i = 0; i < nb; ++i) {
+		if (checkFirstChar(titles[i]) == 1 
+			|| (checkStr(titles[i]) == 1)
+			|| (checkFirstChar(titles[i]) == 1 && checkStr(titles[i]) == 1)
+			|| titles[i] == "\0")
+			std::cout << "NO" << std::endl;
+		else
+			std::cout << "YES" << std::endl;
 	}
 
 	return 0;
