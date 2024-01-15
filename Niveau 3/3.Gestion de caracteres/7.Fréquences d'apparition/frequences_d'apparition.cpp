0d15e7fd72e1a2e0c61681d09273d0f1e4e49d6b
balbader <basilalbapro@gmail.com>
2024-01-15 09:45:25 +0100

    WIP

--- Niveau 3/3.Gestion de caracteres/7.Fréquences d'apparition/frequences_d'apparition.cpp
+++ Niveau 3/3.Gestion de caracteres/7.Fréquences d'apparition/frequences_d'apparition.cpp
@@ -3,15 +3,12 @@
 #include <string>
 #include <cctype>
 
-
-// Count the total amount of letters in a given text
 int countTotLetters(const std::string& text) {
+
     int letter_count = 0;
 
-    // Iterate over each character in the text
     for (char c : text) {
         if (std::isalpha(c)) {
-            // Increment the count for this letter
             letter_count++;
         }
     }
@@ -19,19 +16,12 @@ int countTotLetters(const std::string& text) {
     return letter_count;
 }
 
-// Count the tot occurrence of each letter and return a vector
-// representing the tot amount of occurrences of
-// each letter of the alphabet in a given text
 std::vector<int> countEachLetter(const std::string& text) {
 
-    // Initialize a vector of 26 integers to store the counts for each letter (a-z)
     std::vector<int> letter_count(26, 0);
 
-    // Iterate over each character in the text
     for (char ch : text) {
         if (std::isalpha(ch)) {
-            // Increment the count for this letter
-            // Convert char to lowercase and then to an index (0-25)
             letter_count[std::tolower(ch) - 'a']++;
         }
     }
@@ -41,7 +31,6 @@ std::vector<int> countEachLetter(const std::string& text) {
 
 int main() {
 
-    // initialize a vector and fill it with alphabet letters
     std::vector<char> alphabet;
      for (char letter = 'a'; letter <= 'z'; letter++) {
         alphabet.push_back(letter);
@@ -50,7 +39,6 @@ int main() {
     std::string text;
     std::getline(std::cin, text);
 
-	// count the tot amount of letters in the string
     int letterCount = countTotLetters(text);
     std::cout << "Letter count :" << letterCount << std::endl;
 
@@ -63,7 +51,6 @@ int main() {
 		std::cout << tmp << std::endl;
         ++c;
     }
-
 
     return 0;
 }
