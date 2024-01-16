a9ed8f67d18049a06559b4a0d2fbf09c0fefb4b1
balbader <basilalbapro@gmail.com>
2024-01-16 01:24:06 +0100

    WIP

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversionDeDictionnaire.cpp
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversionDeDictionnaire.cpp
@@ -4,21 +4,24 @@
 #include <vector>
 
 int main(void) {
-  int nb;
-  std::string tmp;
-  std::vector<std::string> langs;
-  std::string language;
+	int nb;
+	std::string tmp;
+	std::vector<std::string> langs;
+	std::string language;
 
-  std::cin >> nb;
-  std::cin.ignore();
+	std::cin >> nb;
+	std::cin.ignore();
 
-  for (int i = 0; i < nb; i++) {
-    std::getline(std::cin, tmp);
-    std::istringstream iss(tmp);
-    while (iss >> language)
-      langs.push_back(language);
-    std::cout << langs[i] << std::endl;
-  }
+	for (int i = 0; i < nb; i++) {
 
-  return (0);
+		std::getline(std::cin, tmp);
+		std::istringstream iss(tmp);
+
+		while (iss >> language)
+			langs.push_back(language);
+
+		std::cout << langs[i] << std::endl;
+	}
+
+	return (0);
 }
