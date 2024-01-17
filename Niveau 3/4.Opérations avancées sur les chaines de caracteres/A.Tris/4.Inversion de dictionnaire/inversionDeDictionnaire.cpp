2e7bcd051d0d431dfde04ab31c090867a66ee82c
balbader <basilalbapro@gmail.com>
2024-01-17 07:47:19 +0100

    WIP

--- Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversionDeDictionnaire.cpp
+++ Niveau 3/4.Opérations avancées sur les chaines de caracteres/A.Tris/4.Inversion de dictionnaire/inversionDeDictionnaire.cpp
@@ -3,26 +3,46 @@
 #include <string>
 #include <vector>
 
-int main(void) {
+char* cpyFirstWd(std::string str) {
 
-	int nb;
-	std::string tmp;
-	std::vector<std::string> langs;
-	std::string language;
+	char firstWd[str.length()];
 
-	std::cin >> nb;
-	std::cin.ignore();
-
-	for (int i = 0; i < nb; i++) {
-
-		std::getline(std::cin, tmp);
-
-		std::istringstream iss(tmp);
-		while (iss >> language)
-			langs.push_back(language);
-
-		std::cout << langs[i] << std::endl;
+	for (int i = 0; i < str.length(); ++i) {
+		if (str[i] == ' ') {
+			firstWd[i] = '\0';
+			return firstWd;
+		}
+		firstWd[i] = str[i];
 	}
-
-	return (0);
+	return str;
 }
+
+int main(void) {
+	std::string str = "Hello World";
+	char
+	return 0;
+}
+
+// int main(void) {
+
+// 	int nb;
+// 	std::string tmp;
+// 	std::vector<std::string> langs;
+// 	std::string language;
+
+// 	std::cin >> nb;
+// 	std::cin.ignore();
+
+// 	for (int i = 0; i < nb; i++) {
+
+// 		std::getline(std::cin, tmp);
+
+// 		std::istringstream iss(tmp);
+// 		while (iss >> language)
+// 			langs.push_back(language);
+
+// 		std::cout << langs[i] << std::endl;
+// 	}
+
+// 	return (0);
+// }
