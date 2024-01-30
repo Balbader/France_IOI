fad547087b7c5ce9f94eeb76312723e7d79b4a77
Basil ALBADER <baalbade@student.42.fr>
2024-01-30 13:18:02 +0100

    WIP

--- Niveau 3/5.Tableaux avancés/1.Infographie/infographie.cpp
+++ Niveau 3/5.Tableaux avancés/1.Infographie/infographie.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 
+void printChar(int l1, int c1, int l2, int c2, char c, int j)
+{
+    if (j >= l1 && j <= l2 && j >= c1 && j <= c2)
+        std::cout << c;
+    // else
+        // std::cout << ".";
+}
+
 int main()
 {
 
     int lines;
     int col;
-    // int totRectangles;
+    int rectangles;
 
-    // int l1, c1, l2, c2;
+    int l1, c1, l2, c2;
 
-    std::cin >> lines;
-    std::cin >> col;
+    char c;
+
+    std::cin >> lines >> col;
+    std::cin >> rectangles;
+    std::cin.ignore();
+
+    for (int i = 0; i < rectangles; i++) {
+        std::cin >> l1 >> c1 >> l2 >> c2 >> c;
+        // std::cout << "entries: " << l1 << " " << c1 << " " << l2 << " " << c2 << " " << c << std::endl;
+    }
 
     char grid[lines][col];
 
     for (int i = 0; i < lines; i++) {
         for (int j = 0; j < col; j++) {
             grid[i][j] = '.';
-            // std::cout << grid[i][j];
+            std::cout << grid[i][j];
+            printChar(l1, c1, l2, c2, c, j);
         }
-        // std::cout << std::endl;
+        std::cout << std::endl;
     }
     return 0;
 }
