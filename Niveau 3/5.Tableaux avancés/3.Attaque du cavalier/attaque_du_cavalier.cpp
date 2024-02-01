b5fa52fd17950ad9b138a6d8a34f3adda3c0fe7c
balbader <basilalbapro@gmail.com>
2024-02-01 19:08:57 +0100

    WIP

--- Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.cpp
+++ Niveau 3/5.Tableaux avancés/3.Attaque du cavalier/attaque_du_cavalier.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-	string grid[8];
+	std::string grid[8];
 	int x[8] = {2, 2, -2, -2, 1, -1, 1, -1};
 	int y[8] = {1, -1, 1, -1, 2, 2, -2, -2};
 
@@ -13,7 +14,7 @@ int main()
 	bool ans = false;
 	for (int i = 0; i < 8; ++i) {
 		for (int j = 0; j < 8; ++j) {
-			if (grid[i][j] == 'C') {
+			if (grid[i][j] == 'c') {
 				for (int k = 0; k < 8; ++k) {
 					if (i + x[k] >= 0 && i + x[k] < 8 && j + y[k] >= 0 && j + y[k] < 8) {
 						if (grid[i + x[k]][j + y[k]] >= 'a' && grid[i + x[k]][j + y[k]] >= 'z')
